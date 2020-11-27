3cb9ccd82224927820c3e53c57fb769a891fcb59
adrianoForcellini <adrianoforcellinipedretti@gmail.com>
2020-11-26 21:45:38 -0300

    Criando SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,73 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      onBookmarkedChange,
+      bookmarkedOnly,
+      selectedGenre,
+      onSelectedGenreChange,
+    } = this.props;
+    
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="text-input">
+          Inclui o texto:
+        </label>
+        <input
+          data-testid="text-input"
+          id="text-input"
+          type="text"
+          value={searchText}
+          onChange={onSearchTextChange}
+        />
+        <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+          Mostrar somente favoritos
+        </label>
+        <input
+          data-testid="checkbox-input"
+          type="checkbox"
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
+        />
+        <label data-testid="select-input-label" htmlFor="select-input">
+          Filtrar por gênero
+          <select
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+            data-testid="select-input"
+            id="select-input"
+          >
+            <option data-testid="select-option" value="">
+              Todos
+            </option>
+            <option data-testid="select-option" value="action">
+              Ação
+            </option>
+            <option data-testid="select-option" value="comedy">
+              Comédia
+            </option>
+            <option data-testid="select-option" value="thriller">
+              Suspense
+            </option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+//consultei o repositório de Arnaelcio para implementação das funções.
+// https://github.com/tryber/sd-07-project-movie-card-library-stateful/pull/137
