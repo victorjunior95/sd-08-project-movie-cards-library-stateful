5e9aa7dc3765801d22471b98031e5b26d4414e37
adrianoForcellini <adrianoforcellinipedretti@gmail.com>
2020-12-07 15:35:02 -0300

    correçao do hadlechange, da posiçao do input(eslint), e do funcionamento do value com this.state com object destructuring

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
 export default class AddMovie extends React.Component {
-  constructor(props) {
-    super(props);
-
+  constructor() {
+    super();
     this.handleChange = this.handleChange.bind(this);
-
     this.state = {
       subtitle: '',
       title: '',
@@ -16,44 +14,47 @@ export default class AddMovie extends React.Component {
     };
   }
 
-  handleChange({ name, value }) {
-    this.setState( { [name]: value });
-  }
 
   render() {
+    const {
+      title,
+      subtitle,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    } = this.state;
+
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="title-input">
           Título
+          <input data-testid="title-input" id="title-input" value={title} onChange={this.handleChange} />
         </label>
-        <input data-testid="title-input" id="title-input"  value={this.state.title} onChange={this.handleChange}
-/>
 
         <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
         Subitítulo
+          <input data-testid="subtitle-input" id="subtitle-input" value={subtitle} onChange={this.handleChange} />
         </label>
-        <input data-testid="subtitle-input" id="subtitle-input"  value={this.state.subtitle} onChange={this.handleChange} />
 
         <label data-testid="image-input-label" htmlFor="image-input">
           Imagem
+          <input data-testid="image-input" id="image-input" value={imagePath} onChange={this.handleChange} />
         </label>
-        <input data-testid="image-input" id="image-input" value={this.state.imagePath} onChange={this.handleChange} />
 
-        <label data-testid="storyline-input-label" htmlFor="storyline-input" value={this.state.storyline} onChange={this.handleChange}>
+        <label data-testid="storyline-input-label" htmlFor="storyline-input" value={storyline} onChange={this.handleChange}>
           Sinopse
+          <input data-testid="storyline-input" id="storyline-input" value={storyline} onChange={this.handleChange} />
         </label>
-        <input data-testid="storyline-input" id="storyline-input" value={this.state.storyline} onChange={this.handleChange}/>
 
-        <label data-testid="rating-input-label" htmlFor="rating-input" >
+        <label data-testid="rating-input-label" htmlFor="rating-input">
           Avaliação
-
+          <input data-testid="rating-input" id="rating-input" type="number" value={rating} onChange={this.handleChange} />
         </label>
-        <input
-          data-testid="rating-input" id="rating-input" type="number" value={this.state.rating} onChange={this.handleChange} />
 
         <label data-testid="genre-input-label" htmlFor="selected-genre-input">
           Gênero
-          <select onChange={''} id="selected-genre-input">
+          <select onChange="" id="selected-genre-input">
             <option data-testid="genre-option" value="action">
               Ação
             </option>
@@ -65,9 +66,9 @@ export default class AddMovie extends React.Component {
             </option>
           </select>
         </label>
-        <input data-testid="genre-input" value={this.state.genre} />
+        <input data-testid="genre-input" value={genre} />
 
-        <button data-testid="send-button">Adicionar filme</button>
+        <button type="button" data-testid="send-button">Adicionar filme</button>
       </form>
     );
   }
