7f944dae20e9f71de8efb57ac3f1ab6a0bb449e0
Leonardo <lecastro16@gmail.com>
2021-01-18 16:06:52 -0300

    structuring the new components v0

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,12 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+//   constructor() {
+//     super();
+//   }
+  render() {
+    return (<div>AddMovie</div>);
+  }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
 class MovieLibray extends React.Component {
-//   constructor(){
-//     super();
-//   }
+  //   constructor(){
+  //     super();
+  //   }
 
   render() {
-    return (<div>Leonardo</div>);
+    const { movies } = this.props;
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
   }
 }
+
+MovieLibray.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    map: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default MovieLibray;
