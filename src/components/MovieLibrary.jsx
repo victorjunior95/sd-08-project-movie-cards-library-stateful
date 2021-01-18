3e5ccb1abc5d6b2e84c60279e89b830b1b24304a
TandyFW <tandyfw@gmail.com>
2021-01-18 16:07:35 -0300

    Requisito 1 lint_1

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,2 +1,1 @@
 // implement MovieLibrary component here
-import React from 'react';
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,14 +3,22 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-
     const { searchText, onSearchTextChange, bookmarkedOnly,
       onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
 
-    return(
-      <input type='text'></input>
+    return (
+      <form data-testid="search-bar-form" />
     );
   }
 }
-
+/*
+SearchBar.propTypes = {
+  searchText: PropType.string.isRequire,
+  onSearchTextChange: PropType.func.isRequire,
+  bookmarkedOnly: PropType.Bool.isRequire,
+  onBookmarkedChange: PropType.func.isRequire,
+  selectedGenre: PropType.string.isRequire,
+  onSelectedGenreChange: PropType.func.isRequire,
+};
+*/
 export default SearchBar;
