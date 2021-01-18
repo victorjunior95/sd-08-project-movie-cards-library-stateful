e23687136e0dde4f1d5844ed0721483b4e228108
rafasysop <rafaelcostamoura@gmail.com>
2021-01-18 17:27:43 -0300

    erro data-form requisito 07

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -5,17 +5,35 @@ class AddMovie extends Component {
     super();
     this.state = {
       title: '',
+      subtitle: '',
     };
   }
 
-  render() {
+  addMovieTitle() {
     const { title } = this.state;
     return (
-      <form>
-        <label data-testid="title-input-label" htmlFor="title-input">
-          Título
-          <input id="title-input" data-testid="title-input" onChange={ title } />
-        </label>
+      <label data-testid="title-input-label" htmlFor="title-input">
+        Título
+        <input id="title-input" data-testid="title-input" onChange={ title } />
+      </label>
+    );
+  }
+
+  addMovieSubtitle() {
+    const { subtitle } = this.state;
+    return (
+      <label data-testid="title-input-label" htmlFor="title-input">
+        Título
+        <input id="title-input" data-testid="title-input" onChange={ subtitle } />
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        { this.addMovieTitle }
+        { this.addMovieSubtitle }
       </form>
     );
   }
