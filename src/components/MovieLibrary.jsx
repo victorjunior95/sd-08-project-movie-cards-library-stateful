3213cbf3e92c43768b46b46999c08789984fd87e
Leonardo <lecastro16@gmail.com>
2021-01-18 15:32:45 -0300

    leonardo-sardinha-movie-cards-library-stateful

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,16 @@
-// implement MovieLibrary component here
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibray extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (<div>Leonardo</div>)
+    }
+}
+
+export default MovieLibray;
