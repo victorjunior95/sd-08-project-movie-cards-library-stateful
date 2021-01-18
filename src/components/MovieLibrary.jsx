0345c87ceb8995a0406cf64e87c659d5a0fe4583
Leonardo <lecastro16@gmail.com>
2021-01-18 15:39:50 -0300

    ajustando o lint do commit inicial

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import MovieList from './MovieList';
-import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import MovieList from './MovieList';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
 
 class MovieLibray extends React.Component {
-    constructor(props){
-        super(props);
-    }
+//   constructor(){
+//     super();
+//   }
 
-    render() {
-        return (<div>Leonardo</div>)
-    }
+  render() {
+    return (<div>Leonardo</div>);
+  }
 }
 
 export default MovieLibray;
