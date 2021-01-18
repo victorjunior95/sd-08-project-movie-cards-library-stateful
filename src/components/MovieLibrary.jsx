74cde77f384c047215a420dfde9e7ca29e550fd1
Hugo Andrade Braga <hugobrr@gmail.com>
2021-01-18 16:35:45 -0300

    Iniciando o projeto com tudo agora vai

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,26 @@
 // implement MovieLibrary component here
+// mais um comentario aqui pra lanxar minha beforeEachUnitTest
+
+import React, {Component} from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import addMovie from './addMovie';
+
+class MovieList extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h2> My awesome movie library</h2>
+                <SearchBar />
+                <MovieList movies={this.props.movies} />
+                <addMovie />
+            </div>
+        );
+    }
+}
+
+export default MovieLibrary;
