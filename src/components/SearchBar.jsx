c174df334f54bbe2d42a527b95046ad5ffebb87d
Humberto Bandeira <humberto-bandeira@hotmail.com>
2021-01-18 19:33:02 -0400

    Requisito 4 concluido

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,25 +3,50 @@ import React from 'react';
 import Proptypes from 'prop-types';
 
 class SeachBar extends React.Component {
+  renderInput(searchText, onSearchTextChange) {
+    return (
+      <label data-testid="text-input-label" htmlFor="searchInput">
+        Inclui o texto:
+        <input
+          data-testid="text-input"
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          id="searchInput"
+        />
+      </label>
+    );
+  }
+
+  renderCheckBox(bookmarkedOnly, onBookmarkedChange) {
+    return (
+      <label htmlFor="checkboxFilter" data-testid="checkbox-input-label">
+        Mostrar somente favoritos
+        <input
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+        />
+      </label>
+    );
+  }
+
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
       selectedGenre,
       onSelectedGenreChange } = this.props;
     return (
-      <div
-        searchText={ searchText }
+      <form
+        data-testid="search-bar-form"
         bookmarkedOnly={ bookmarkedOnly }
         onBookmarkedChange={ onBookmarkedChange }
         selectedGenre={ selectedGenre }
         onSelectedGenreChange={ onSelectedGenreChange }
       >
-        <form data-testid="search-bar-form">
-          <label data-testid="text-input-label">
-            Inclui o texto:
-            <input data-testid="text-input" type="text" value={ searchText } onChange={ onSearchTextChange } />
-          </label>
-        </form>
-      </div>
+        {this.renderInput(searchText, onSearchTextChange)}
+        {this.renderCheckBox(bookmarkedOnly, onBookmarkedChange)}
+      </form>
     );
   }
 }
