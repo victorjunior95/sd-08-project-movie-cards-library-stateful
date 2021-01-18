6ba6e640d96d2c45bb925bcb6a3c6a66fd3f5f19
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-18 20:49:46 -0300

    Requisito 8 concluido

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,9 +2,25 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
+  renderTitleInput() {
+    return (
+      <label htmlFor="title-input" data-testid="title-input-label">
+        Título
+        <input
+          type="text"
+          id="title-input"
+          name="title-input"
+          data-testid="title-input"
+        />
+      </label>
+    );
+  }
+
   render() {
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        { this.renderTitleInput() }
+      </form>
     );
   }
 }
