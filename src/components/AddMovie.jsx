d9392caa076212e3e6d62e8e0130eec850a80f29
rafasysop <rafaelcostamoura@gmail.com>
2021-01-18 17:43:36 -0300

    addMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -9,12 +9,31 @@ class AddMovie extends Component {
     };
   }
 
+  handleTitle(e) {
+    const newTitle = e.target.value;
+    this.setState({
+      title: newTitle,
+    });
+  }
+
+  handleSubtitle(e) {
+    const newValue = e.target.value;
+    this.setState({
+      subtitle: newValue,
+    });
+  }
+
   addMovieTitle() {
     const { title } = this.state;
     return (
       <label data-testid="title-input-label" htmlFor="title-input">
         Título
-        <input id="title-input" data-testid="title-input" onChange={ title } />
+        <input
+          id="title-input"
+          data-testid="title-input"
+          value={ title }
+          onChange={ this.handleTitle }
+        />
       </label>
     );
   }
@@ -22,9 +41,14 @@ class AddMovie extends Component {
   addMovieSubtitle() {
     const { subtitle } = this.state;
     return (
-      <label data-testid="title-input-label" htmlFor="title-input">
+      <label data-testid="subtitle-input-label" htmlFor="subtitle">
         Título
-        <input id="title-input" data-testid="title-input" onChange={ subtitle } />
+        <input
+          id="subtitle"
+          value={ subtitle }
+          data-testid="subtitle-input"
+          onChange={ this.handleSubtitle }
+        />
       </label>
     );
   }
