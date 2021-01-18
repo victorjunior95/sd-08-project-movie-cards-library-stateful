b5e65dd474b699f15209b224f18e8e907c85685a
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2021-01-18 17:34:32 -0300

    Iniciando o projeto :rocket:

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,37 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import MovieList from './MovieList';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        {/* <SearchBar /> */}
+        <MovieList movies={ movies } />
+        {/* <AddMovie /> */}
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    map: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default MovieLibrary;
