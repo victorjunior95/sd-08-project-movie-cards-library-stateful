7535fb65497150bafd7b3491891b944b1952829c
wend3ll-souza <wend3llsouza@gmail.com>
2021-01-18 16:37:24 -0300

    :v: Iniciando Projeto

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement SearchBar component here
+import React from 'React';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <section>
+
+      </section>
+    );
+  }
+}
+
+export default SearchBar;
