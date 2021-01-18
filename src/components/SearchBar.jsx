ee73e33aab86f3f7e51e11870265b225e10e73ec
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-18 16:14:38 -0300

    Req03: render an input type text inside <SearchBar /> form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,16 +3,24 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookMarkedOnly, onBookMarkedChang, selectedGenre, onSelectedGenreChange } = this.props;
+    const { searchText, onSearchTextChange, bookMarkedOnly } = this.props;
+    const {onBookMarkedChang, selectedGenre, onSelectedGenreChange } = this.props;
 
     return(
-      <form data-testid="search-bar-form" />
+      <form data-testid="search-bar-form">
+        <label
+          data-testid="text-input-label"
+          value={ searchText }
+          onChange={ onSearchTextChange }>
+          <input type="text" data-testid="text-input" />
+        </label>
+      </form>
     );
   }
 }
 
 SearchBar.proptypes = {
-  searchText: PropTypes.string,
+  searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func,
   bookMarkedOnly: PropTypes.bool,
   onBookMarkedChang: PropTypes.func,
