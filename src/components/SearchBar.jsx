9cb76604958c14486a58fc4ebcd1e58a9952cac2
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-18 20:33:11 -0300

    Requisito 5 concluído

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -36,11 +36,33 @@ class SearchBar extends Component {
     );
   }
 
+  renderSelect() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label htmlFor="select-input" data-testid="select-input-label">
+        Filtrar por gênero
+        <select
+          name="select-input"
+          id="select-input"
+          data-testid="select-input"
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+        >
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     return (
       <form data-testid="search-bar-form">
         { this.renderTextInput() }
         { this.renderCheckBox() }
+        { this.renderSelect() }
       </form>
     );
   }
@@ -51,6 +73,8 @@ SearchBar.propTypes = {
   onSearchTextChange: PropTypes.func.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
   onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
