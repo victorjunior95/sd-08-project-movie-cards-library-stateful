89aa0a8e44057599f8e8fe4878558d1f11a2217d
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-01-18 15:27:54 -0300

    teste

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,2 +1,21 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText=""
+          onSearchTextChange={ AS }
+          bookmarkedOnly
+          onBookmarkedChange={ AS }
+          selectedGenre="oi"
+          onSelectedGenreChange={ W }
+        />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="select" />
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
