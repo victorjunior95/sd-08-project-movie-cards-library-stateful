50c3e9ecc4a88523a7838aefe92cba5acd80442d
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2021-01-18 11:48:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import MovieCardList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieCardList movies={ movies } />
     </div>
   );
 }
