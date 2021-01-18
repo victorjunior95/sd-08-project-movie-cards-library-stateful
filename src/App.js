515211ec238114eedf24b5843fe5ba47dbe2a46b
emassaki <massakihirayama@gmail.com>
2021-01-18 17:30:45 -0300

    Finalizado primeiro componente - SearchBar (requisitos 1 a 5)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import SearchBar from './components/SearchBar';
 import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
       <MovieList movies={ movies } />
     </div>
   );
