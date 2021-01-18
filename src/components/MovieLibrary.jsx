55703249b5f03d5c3429fcdc40e8c73a1b7d1dd4
Ediberto Oliveira <edibertooliveira@aol.com>
2021-01-18 17:22:05 -0300

    craeted <MovieLibrary/> requirement 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,17 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
