7b1c48f74ef6fc0e4dbd9b54e500ae86db8e55f9
Ediberto Oliveira <edibertooliveira@aol.com>
2021-01-18 17:51:56 -0300

    craeted state MovieLibrary requirement 16 e 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -4,11 +4,32 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: {},
+    };
+  }
+
+  handleAddedState(state) {
+    this.setState({
+      movies: state,
+    });
+  }
+
   render() {
+    const { bookmarkedOnly, searchText, selectedGenre } = this.state;
     return (
       <div>
-        <SearchBar />
-        <AddMovie />
+        <SearchBar
+          bookmarkedOnly={ bookmarkedOnly }
+          searchText={ searchText }
+          selectedGenre={ selectedGenre }
+        />
+        <AddMovie onClick={ this.handleAddedState } />
       </div>
     );
   }
