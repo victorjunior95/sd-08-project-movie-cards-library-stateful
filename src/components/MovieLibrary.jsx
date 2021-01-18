836a965c8c3f1730abcf8e3c2c7142337ed779ec
rafasysop <rafaelcostamoura@gmail.com>
2021-01-18 15:54:58 -0300

    1 e segundo requisito

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,46 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends Component {
+  constructor() {
+    super();
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
+  onSearchTextChange(e) {
+    console.log(e.target);
+  }
+
+  onBookmarkedChange(e) {
+    console.log(e.target);
+  }
+
+  onSelectedGenreChange(e) {
+    console.log(e.target);
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    return (
+      <div>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSearchTextChange={ this.onSearchTextChange }
+          onBookmarkedChange={ this.onBookmarkedChange }
+          onSelectedGenreChange={ this.onSelectedGenreChange }
+        />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,33 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input-label">
+          Inclui o texto:
+          <input
+            id="text-input-label"
+            type="text"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+            data-testid="text-input"
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.function,
+};
+
+SearchBar.defaultProps = {
+  searchText: '',
+};
+
+export default SearchBar;
