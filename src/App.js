f1810649a51b0008cd11f00a36a0170efaa13b5c
Bruno Martins <bruno.martins217@gmail.com>
2021-01-18 15:35:16 -0300

    First Try Requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <MovieLibrary />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly, selectedGenre, onSelectedGenreChange } = this.props;
+    return (<div>
+      <form data-testid="search-bar-form" action="" />
+    </div>);
+  }
+}
+
+export default SearchBar;
