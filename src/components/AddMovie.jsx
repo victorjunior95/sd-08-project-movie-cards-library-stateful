e1cf915f2a0a4146ee5908171f1ca174faf476e8
Daniel <danieldsrd@gmail.com>
2021-01-18 17:47:30 -0300

    Requisitos do 6 ao 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,147 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  changeHandler(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  newTitle() {
+    const { title } = this.state;
+    return (
+      <label htmlFor="title-input" data-testid="title-input-label">
+        Título
+        <input
+          type="text"
+          data-testid="title-input"
+          value={ title }
+          onChange={ this.changeHandler }
+          name="title"
+        />
+      </label>
+    );
+  }
+
+  newSubtitle() {
+    const { subtitle } = this.state;
+    return (
+      <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+        Subtítulo
+        <input
+          type="text"
+          data-testid="subtitle-input"
+          value={ subtitle }
+          onChange={ this.changeHandler }
+          name="subtitle"
+        />
+      </label>
+    );
+  }
+
+  newImage() {
+    const { imagePath } = this.state;
+    return (
+      <label htmlFor="image-input" data-testid="image-input-label">
+        Imagem
+        <input
+          type="text"
+          data-testid="image-input"
+          value={ imagePath }
+          onChange={ this.changeHandler }
+          name="imagePath"
+        />
+      </label>
+    );
+  }
+
+  newSynopsis() {
+    const { storyline } = this.state;
+    return (
+      <label htmlFor="storyline-input" data-testid="storyline-input-label">
+        Sinopse
+        <textarea
+          type="text"
+          data-testid="storyline-input"
+          value={ storyline }
+          onChange={ this.changeHandler }
+          name="storyline"
+        />
+      </label>
+    );
+  }
+
+  newRating() {
+    const { rating } = this.state;
+    return (
+      <label htmlFor="rating-input" data-testid="rating-input-label">
+        Avaliação
+        <input
+          type="number"
+          data-testid="rating-input"
+          value={ rating }
+          onChange={ this.changeHandler }
+          name="rating"
+        />
+      </label>
+    );
+  }
+
+  newGenre() {
+    const { genre } = this.state;
+    return (
+      <label htmlFor="genre-input" data-testid="genre-input-label">
+        Gênero
+        <select
+          name="genre"
+          data-testid="genre-input"
+          value={ genre }
+          onChange={ this.changeHandler }
+        >
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  addMovieForm() {
+    return (
+      <button type="submit">
+        Adicionar filme
+      </button>
+    );
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        { this.newTitle() }
+        { this.newSubtitle() }
+        { this.newImage() }
+        { this.newSynopsis() }
+        { this.newRating() }
+        { this.newGenre() }
+        { this.addMovieForm() }
+      </form>
+
+    );
+  }
+}
+
+export default AddMovie;
