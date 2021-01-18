f995abcc3a5f84ce1b65e2faecac6f683bb5b780
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-01-18 17:33:01 -0300

    renderiza SearchBar em MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,59 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+import SearchBar from './SearchBar';
+
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies
+    }
+  }
+
+  onSearchTextChange = ({ target }) => {
+    const { value } = target;
+
+    this.setState({
+      searchText: value
+    })
+  }
+
+  onBookmarkedChange = ({ target }) => {
+    const { checked } = target;
+
+    this.setState({
+      bookmarkedOnly: checked
+    })
+  }
+
+  onSelectedGenreChange = ({ target }) => {
+    const { value } = target;
+
+    this.setState({
+      selectedGenre: value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText={this.state.searchText}
+          onSearchTextChange={this.onSearchTextChange}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={this.onBookmarkedChange}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={this.onSelectedGenreChange}
+        />
+
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
