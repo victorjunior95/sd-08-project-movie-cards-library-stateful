c458f5ea2214a3a9363fc1ada0aa4b82ba9fea59
rafasysop <rafaelcostamoura@gmail.com>
2021-01-18 17:21:36 -0300

    renderizando add movie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+    };
+  }
+
   render() {
+    const { title } = this.state;
     return (
-      <div>
-        oi
-      </div>
+      <form>
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input id="title-input" data-testid="title-input" onChange={ title } />
+        </label>
+      </form>
     );
   }
 }
