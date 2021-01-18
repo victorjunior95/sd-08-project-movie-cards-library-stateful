b5bc058e79061cba643ac0a2882388552994c414
Ediberto Oliveira <edibertooliveira@aol.com>
2021-01-18 14:53:44 -0300

    iniciando o projeto. VAMOS COM TUDO

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,14 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
