cad8f8b25acac399cfbccc20bc9f62adc2afaad1
rafasysop <rafaelcostamoura@gmail.com>
2021-01-18 20:09:51 -0300

    Requisito 16 e 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.addNewMovie = this.addNewMovie.bind(this);
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
+      movies: [...movies],
     };
   }
 
   onSearchTextChange(e) {
-    console.log(e.target);
+    this.setState({
+      searchText: e.target.value,
+    });
   }
 
   onBookmarkedChange(e) {
-    console.log(e.target);
+    this.setState({
+      bookmarkedOnly: e.target.value,
+    });
   }
 
   onSelectedGenreChange(e) {
-    console.log(e.target);
+    this.setState({
+      selectedGenre: e.target.value,
+    });
+  }
+
+  addNewMovie(e) {
+    const { movies } = this.state;
+    this.setState({
+      movies: [...movies, e.target.value],
+    });
+  }
+
+  renderMovies() {
+    const { movies } = this.state;
+    return (
+      <MovieList movies={ movies } />
+    );
   }
 
   render() {
@@ -38,9 +64,12 @@ class MovieLibrary extends Component {
           onBookmarkedChange={ this.onBookmarkedChange }
           onSelectedGenreChange={ this.onSelectedGenreChange }
         />
+        { this.renderMovies() }
+        <AddMovie onClick={ this.addNewMovie } />
       </div>
     );
   }
 }
 
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.string).isRequired };
 export default MovieLibrary;
