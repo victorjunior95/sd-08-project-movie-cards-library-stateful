3ad36595c505a8ffcda8beb10469b466b48df532
wesleypribeiro <wesley_paulo00@hotmail.com>
2021-01-18 18:18:36 -0300

    Iniciando o projeto

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,5 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
