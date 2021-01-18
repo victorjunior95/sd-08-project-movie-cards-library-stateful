30d812bbf5c7d7c82d38e618b465db38a48bfe88
Mario Pardo <mariio.pardojr@gmail.com>
2021-01-18 16:08:15 -0400

    Correcao eslint App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 import SearchBar from './components/SearchBar';
-import movies from './data';
+// import movies from './data';
 
 import Header from './components/Header';
 
