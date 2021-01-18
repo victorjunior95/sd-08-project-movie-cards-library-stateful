7ca75f853d5af01c8e5c564f6d4e8db28846d65d
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-01-18 17:22:30 -0300

    Req 1 e 2, tested and passed!

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,39 @@
 // implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: true,
+      selectedGenre: '',
+    };
+  }
+
+  onSearchTextChange() {
+    const { searchText } = this.state;
+    console.log({ searchText });
+  }
+
+  onBookmarkChange() {
+    const { bookmarkedOnly } = this.state;
+    console.log(bookmarkedOnly);
+  }
+
+  onSelectGenderChange() {
+    const { selectedGenre } = this.state;
+    console.log({ selectedGenre });
+  }
+
+  render() {
+    return (
+      <section>
+        <input type="text" name="" id="" data-testid="search-bar-form" />
+      </section>
+    );
+  }
+}
+
+export default SearchBar;
