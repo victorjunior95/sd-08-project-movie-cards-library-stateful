0b6d316e36612f0c717e8985fabb620cb540a5a7
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-01-18 20:27:12 -0300

    Req 3, OK

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,21 @@
 // implement SearchBar component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      searchText: '',
+      searchText: 'My Text',
       bookmarkedOnly: true,
       selectedGenre: '',
     };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
   }
 
-  onSearchTextChange() {
+  onSearchTextChange(event) {
+    this.setState({ searchText: event.target.value });
     const { searchText } = this.state;
     console.log({ searchText });
   }
@@ -28,12 +31,28 @@ class SearchBar extends React.Component {
   }
 
   render() {
+    const { onSearchTextChange, searchText } = this.props;
     return (
-      <section>
-        <input type="text" name="" id="" data-testid="search-bar-form" />
-      </section>
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto
+          <input
+            type="text"
+            name="text-input"
+            id="text-input"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+      </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
