63578e15f3c501bc5e9919cd3c75f2ae7c2876c6
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-01-19 17:06:52 -0300

    Req 11, OK

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -8,13 +8,14 @@ class AddMovie extends React.Component {
       subtitle: '',
       title: '',
       imagePath: '',
-      // storyline: '',
+      storyline: '',
       // rating: 0,
       // genre: 'action',
     };
     this.handleTitleInput = this.handleTitleInput.bind(this);
     this.handleSubtitleInput = this.handleSubtitleInput.bind(this);
     this.handleImageInput = this.handleImageInput.bind(this);
+    this.handleStorylineInput = this.handleStorylineInput.bind(this);
   }
 
   handleTitleInput(event) {
@@ -33,6 +34,12 @@ class AddMovie extends React.Component {
     const { imagePath } = this.state;
     this.setState({ imagePath: event.target.value });
     console.log(imagePath);
+  }
+
+  handleStorylineInput(event) {
+    const { storyline } = this.state;
+    this.setState({ storyline: event.target.value });
+    console.log(storyline);
   }
 
   titleInputLabel(title, handleTitleInput) {
@@ -83,14 +90,32 @@ class AddMovie extends React.Component {
     );
   }
 
+  storylineInputLabel(storyline, handleStorylineInput) {
+    return (
+      <label htmlFor="input" data-testid="storyline-input-label">
+        Sinopse
+        <textarea
+          type="text"
+          name="storyline-input"
+          id="storyline-input"
+          data-testid="storyline-input"
+          value={ storyline }
+          onChange={ handleStorylineInput }
+        />
+      </label>
+    );
+  }
+
   render() {
-    const { title, subtitle, imagePath } = this.state;
-    const { handleTitleInput, handleSubtitleInput, handleImageInput } = this;
+    const { title, subtitle, imagePath, storyline } = this.state;
+    const { handleTitleInput, handleSubtitleInput,
+      handleImageInput, handleStorylineInput } = this;
     return (
       <form action="" data-testid="add-movie-form">
         {this.titleInputLabel(title, handleTitleInput)}
         {this.subtitleInputLabel(subtitle, handleSubtitleInput)}
         {this.imageInputLabel(imagePath, handleImageInput)}
+        {this.storylineInputLabel(storyline, handleStorylineInput)}
       </form>
     );
   }
