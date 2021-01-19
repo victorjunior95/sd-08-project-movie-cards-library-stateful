871a0420e37cd48161458d09cccab0955072599a
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-19 09:29:43 -0300

    Requisito 15 concluido

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,10 @@
 // implement MovieLibrary component here
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  render() {
+    return <p>Movie Library</p>;
+  }
+}
+
+export default MovieLibrary;
