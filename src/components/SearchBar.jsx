e0d2d8e85ec5001bc4f3a19f91924683e5623bf7
albertassi88 <albertassi88@gmail.com>
2021-01-19 17:50:57 -0300

    Alteracoes nos componentes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,47 +1,47 @@
 import React from 'react';
 
-class SearchBar extends React.Component {
-  
+class SearchBar extends React.Component {  
   inputText(searchText, onSearchTextChange) {
     return (
-    <label data-testid="text-input-label">
-          Inclui o texto:
-          <input
-            type="text"
-            name="searchText"
-            id="searchText"
-            value={ searchText }
-            onChange={ onSearchTextChange }
-            data-testid="text-input"
-            className="input"
-          />
-    </label>
+      <label data-testid="text-input-label" htmlFor="searchText">
+        Inclui o texto:
+        <input
+          type="text"
+          name="searchText"
+          id="searchText"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+          className="input"
+        />
+      </label>
     );
   }
 
   inputCheckBox(bookmarkedOnly, onBookmarkedChange) {
     return (
-      <label data-testid="checkbox-input-label">
-          Mostrar somente favoritos
-          <input
-            type="checkbox"
-            name="bookmarkedOnly"
-            id="bookmarkedOnly"
-            checked={ bookmarkedOnly }
-            onChange={ onBookmarkedChange }
-            data-testid="checkbox-input"
-            className="checkbox-input"
-          />
+      <label data-testid="checkbox-input-label" htmlFor="bookmarkedOnly">
+        Mostrar somente favoritos
+        <input
+          type="checkbox"
+          name="bookmarkedOnly"
+          id="bookmarkedOnly"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+          className="checkbox-input"
+        />
       </label>
     );
   }
 
   selectGenre(selectedGenre, onSelectedGenreChange) {
     return (
-      <label data-testid="select-input-label">
+      <label data-testid="select-input-label" htmlFor="selectedGenre">
         Filtrar por gênero
         <select
-          name="selectedGenre"          
+          name="selectedGenre"
+          id="selectedGenre"
           value={ selectedGenre }
           onChange={ onSelectedGenreChange }
           data-testid="select-input"
@@ -52,7 +52,7 @@ class SearchBar extends React.Component {
           <option data-testid="select-option" value="comedy">Comédia</option>
           <option data-testid="select-option" value="thriller">Suspense</option>
         </select>
-      </label>    
+      </label>
     );
   }
 
