28bf1e9f80365549fb2c389023cbe446533a5794
victor Farias <victorfarias.tv@gmail.com>
2021-01-19 07:36:50 -0300

    Adicionado requisitos 6, 7, 8, 9, 10, 11

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,81 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleChanges = this.handleChanges.bind(this);
+  }
+
+  handleChanges({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  inputTitle() {
+    const { title } = this.props;
+
+    return (
+      <label data-testid="title-input-label" htmlFor="title-input">
+        Título
+        <input
+          data-testid="title-input"
+          type="text"
+          value={ title }
+          onChange={ this.handleChanges }
+        />
+      </label>
+    );
+  }
+
+  inputSubtitle() {
+    const { subtitle } = this.props;
+
+    return (
+      <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+        Subtítulo
+        <input
+          data-testid="subtitle-input"
+          type="text"
+          value={ subtitle }
+          onChange={ this.handleChanges }
+        />
+      </label>
+    );
+  }
+
+  inputStoryline() {
+    const { storyline } = this.props;
+
+    return (
+      <label data-testid="storyline-input-label" htmlFor="storyline-input">
+        Sinopse
+        <textarea
+          data-testid="storyline-input"
+          value={ storyline }
+          onChange={ this.handleChanges }
+        />
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        { this.inputTitle() }
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
