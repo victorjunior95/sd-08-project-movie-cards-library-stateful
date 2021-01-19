131be8061833e0a6a10541478c0e39716999b510
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-19 00:35:06 -0300

    Requisito 9 concluido

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,7 +2,24 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      'subtitle-input': '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   renderTitleInput() {
+    const { 'title-input': titleInput } = this.state;
     return (
       <label htmlFor="title-input" data-testid="title-input-label">
         Título
@@ -11,6 +28,25 @@ class AddMovie extends Component {
           id="title-input"
           name="title-input"
           data-testid="title-input"
+          value={ titleInput }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  renderSubtitleInput() {
+    const { 'subtitle-input': subtitleInput } = this.state;
+    return (
+      <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+        Subtítulo
+        <input
+          type="text"
+          id="subtitle-input"
+          name="subtitle-input"
+          data-testid="subtitle-input"
+          value={ subtitleInput }
+          onChange={ this.handleChange }
         />
       </label>
     );
@@ -20,6 +56,7 @@ class AddMovie extends Component {
     return (
       <form data-testid="add-movie-form">
         { this.renderTitleInput() }
+        { this.renderSubtitleInput() }
       </form>
     );
   }
