a257b04e7037a3eb558885199e7b63c9546a343c
wesleypribeiro <wesley_paulo00@hotmail.com>
2021-01-19 16:35:54 -0300

    R07, R08, R09 - Concluido

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,38 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      title: event.target.value,
+    });
+  }
+
+  render() {
+    const { title } = this.state;
+
+    return (
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            data-testid="title-input"
+            type="text"
+            value={ title }
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
