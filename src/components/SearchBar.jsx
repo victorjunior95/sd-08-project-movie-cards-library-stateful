db499627a1c6d4046b5a4c62a6403503b3deb2e7
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-01-19 16:34:50 -0300

    req1 a req5, ainda a corrigir eslint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,40 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  testeFuncao(searchText, onSearchTextChange) {
+    return (
+      <label data-testid="text-input-label">
+        Inclui o texto:
+        <input
+          type="text"
+          data-testid="text-input"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+        />
+      </label>
+    );
+  }
+
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        { this.testeFuncao(searchText, onSearchTextChange) }
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } data-testid="checkbox-input" />
+        </label>
+        <label data-testid="select-input-label">
+          Filtrar por gênero
+          <select data-testid="select-input" value={ selectedGenre } onChange={ onSelectedGenreChange }>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
