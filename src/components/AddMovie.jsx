9b025ad45cd1a99c78f7844d68929252e77cb010
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-01-19 10:24:03 -0300

    adicionando requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,9 +2,25 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
   render() {
-    return <div>A</div>;
+    return <div>a</div>;
   }
 }
 
 export default AddMovie;
+// AddMovie.propTypes = {
+//   onClick: PropTypes.func.isRequired,
+// };
