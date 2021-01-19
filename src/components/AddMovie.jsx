43261a701a684a0e04ffff14b0ce3af1357a9699
Igor Nascimento <igorln96@gmail.com>
2021-01-18 23:07:55 -0300

    req 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      title: '',
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  title() {
+    return (
+      <label data-testid="title-input-label" htmlFor="title">
+        Título
+        <input
+          type="text"
+          name="title"
+          data-testid="title-input"
+          onChange={ this.handleChange }
+          id="title"
+        />
+      </label>
+    );
+  }
+
   render() {
     return (
-      <form data-testid="add-movie-form"> 
+      <form data-testid="add-movie-form">
+        {this.title()}
       </form>
     );
   }
