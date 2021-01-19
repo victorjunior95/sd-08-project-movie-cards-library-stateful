71cbecbb0f30030463acc8e5f28ecdc2f959bb5e
Vitor Nunes <vitornunes200@gmail.com>
2021-01-19 19:20:33 -0300

    Requisitos 1 ao 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,81 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      titleText: '',
+      subtitleText: '',
+      image: '',
+    };
+    this.updateTitleText = this.updateTitleText.bind(this);
+  }
+
+  createTitle() {
+    const { titleText } = this.state;
+    return (
+      <label data-testid="title-input-label" htmlFor="titleText">
+        Título
+        <input
+          type="text"
+          title={ titleText }
+          data-testid="title-input"
+          onChange={ this.updateTitleText }
+          id="titleText"
+        />
+      </label>
+    );
+  }
+
+  createSubtitle() {
+    const { subtitleText } = this.state;
+    return (
+      <label data-testid="subtitle-input-label" htmlFor="subtitleText">
+        Subtítulo
+        <input
+          type="text"
+          subtitle={ subtitleText }
+          data-testid="subtitle-input"
+          onChange={ this.updateTitleText }
+          id="subtitleText"
+        />
+      </label>
+    );
+  }
+
+  createImage() {
+    const { image } = this.state;
+    return (
+      <label data-testid="image-input-label" htmlFor="image">
+        Imagem
+        <input
+          type="text"
+          imagePath={ image }
+          data-testid="image-input"
+          onChange={ this.updateTitleText }
+          id="image"
+        />
+      </label>
+    );
+  }
+
+  updateTitleText(event) {
+    const text = event.target.value;
+    const keyName = event.target.id;
+    this.setState({
+      [keyName]: text,
+    });
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        { this.createTitle() }
+        { this.createSubtitle() }
+        { this.createImage() }
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,77 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  createTextInput() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <label data-testid="text-input-label" htmlFor="textInput">
+        Inclui o texto
+        <input
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+          id="textInput"
+        />
+      </label>
+    );
+  }
+
+  createCheckInput() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    return (
+      <label data-testid="checkbox-input-label" htmlFor="checkInput">
+        Mostrar somente favoritos
+        <input
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+          id="checkInput"
+        />
+      </label>
+    );
+  }
+
+  createSelectInput() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label data-testid="select-input-label" htmlFor="selectInput">
+        Filtrar por gênero
+        <select
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+          data-testid="select-input"
+          id="selectInput"
+        >
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        { this.createTextInput() }
+        { this.createCheckInput() }
+        { this.createSelectInput() }
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
