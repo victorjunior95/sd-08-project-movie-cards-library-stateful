8d840274f5bb080f9831a8bb9ee8ce71e65f7147
Victor Miwa <victoryukimiwa@hotmail.com>
2021-01-19 18:14:54 -0300

    SearchBar finalizado

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,58 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  inputText = () => {
+    return (
+      <label data-testid="text-input-label">
+      Inclui o texto:
+      <input
+        type="text"
+        value={ this.props.searchText }
+        onChange={ this.props.onSearchTextChange }
+        data-testid="text-input"
+      />
+    </label>
+    )
+  }
+  
+  inputCheckBox = () => {
+    return (
+      <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input 
+            type="checkbox" 
+            checked={ this.props.bookmarkedOnly } 
+            onChange={ this.props.onBookmarkedChange } 
+            data-testid="checkbox-input" 
+          />
+      </label>
+    )
+  }
+
+  selectForm = () => {
+    return (
+      <label data-testid="select-input-label">
+        Filtrar por gênero
+        <select value={ this.props.selectedGenre } onChange={ this.props.onSelectedGenreChange } data-testid="select-input">
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </label>
+    )
+  }
+
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        { this.inputText() }
+        { this.inputCheckBox() }
+        { this.selectForm() }
+       
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
