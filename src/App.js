f474b69af93d5e8280225f1cd8ddb4af063a5a37
Douglas Ferreira <dodofp.df@gmail.com>
2021-01-19 16:34:40 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 import Header from './components/Header';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,45 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  testeFunction(searchText, onSearchTextChange) {
+    return (
+      <label data-testid="text-input-label">
+        Inclui o texto:
+        <input onChange={onSearchTextChange} value={searchText} data-testid="text-input"
+          type="text"
+        />
+      </label>
+    );
+  }
+  render() {
+    const { 
+      searchText, 
+      onSearchTextChange, 
+      bookmarkedOnly, 
+      onBookmarkedChange,
+      selectedGenre, 
+      onSelectedGenreChange, 
+    } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        {this.testeFunction(searchText, onSearchTextChange)}
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input data-testid="checkbox-input" onChange={onBookmarkedChange} checked={bookmarkedOnly} type="checkbox" />
+        </label>
+        <label data-testid="select-input-label">
+          Filtrar por gênero
+          <select data-testid="select-input" onChange={onSelectedGenreChange} value={selectedGenre}>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
