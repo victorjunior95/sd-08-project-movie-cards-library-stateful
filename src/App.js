0e9430b541940a9663c2135fe1fc4d2d200f18c1
albertassi88 <albertassi88@gmail.com>
2021-01-19 17:27:33 -0300

    Alteracoes nos componentes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <AddMovie />
-      <SearchBar />
+      <SearchBar />     
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,31 +1,48 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  render() {
-    const { searchText, onSearchTextChange } = this.props;
-    const { bookmarkedOnly, onBookmarkedChange } = this.props;
-    const { selectedGenre, onSelectedGenreChange } = this.props;
+  
+  inputText(searchText, onSearchTextChange) {
     return (
-      <form data-testid="search-bar-form">
         <label data-testid="text-input-label">
           Inclui o texto:
           <input onChange={ onSearchTextChange } name="searchText" value={ searchText } type="text" data-testid="text-input" />
         </label>
+    );
+  }
+
+  inputCheckBox(bookmarkedOnly, onBookmarkedChange) {
+    return (
         <label data-testid="checkbox-input-label">
           Mostrar somente favoritos
-            <input onChange={ onBookmarkedChange } checked={ bookmarkedOnly } type="checkbox" data-testid="checkbox-input" />
+          <input onChange={ onBookmarkedChange } checked={ bookmarkedOnly } type="checkbox" data-testid="checkbox-input" />
         </label>
-        <select onChange={ onSelectedGenreChange } value={ selectedGenre } data-testid="select-input">
-          <label data-testid="select-input-label">
-          Filtrar por gênero
-            <input/>
-            <option value={""} data-testid="select-option">Todos</option>
-            <option value={"action"} data-testid="select-option">Ação</option>
-            <option value={"comedy"} data-testid="select-option">Comédia</option>
-            <option value={"thriller"} data-testid="select-option">Suspense</option>
-          </label>
-        </select>
-     </form>
+    );
+  }
+
+  selectGenre(selectedGenre, onSelectedGenreChange) {
+    return (
+      <select onChange={ onSelectedGenreChange } value={ selectedGenre } data-testid="select-input">
+      <label data-testid="select-input-label">
+      Filtrar por gênero
+        <input/>
+        <option value={""} data-testid="select-option">Todos</option>
+        <option value={"action"} data-testid="select-option">Ação</option>
+        <option value={"comedy"} data-testid="select-option">Comédia</option>
+        <option value={"thriller"} data-testid="select-option">Suspense</option>
+      </label>
+    </select>
+    );
+  }
+
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;    
+    return (
+      <form data-testid="search-bar-form">
+        {this.inputText(searchText, onSearchTextChange)}
+        {this.inputCheckBox(bookmarkedOnly, onBookmarkedChange)}
+        {this.selectGenre(selectedGenre, onSelectedGenreChange)}       
+      </form>
     )
   }
 }
