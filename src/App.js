790a3bb615aab5f2a55d8c54659d55e27e25e5b4
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-01-19 18:10:08 -0300

    Req 1 ao 8 finalizados

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar'
+
+import MovieLibrary from './components/MovieLibrary';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
+      <MovieLibrary />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,35 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // subtitle: '',
+      title: '',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+  }
+
+  render() {
+    // const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    const { title } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input
+            value={ title }
+            data-testid="title-input"
+            onChange={ (e) => this.setState({ title: e.target.value }) }
+          />
+        </label>
+
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
