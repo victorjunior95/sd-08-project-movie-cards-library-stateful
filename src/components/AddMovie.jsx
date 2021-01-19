0b1076bae6a9278b8731551c17d7860346d4c293
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-01-19 00:19:53 -0300

    Criaedo componente AddMovie renderizando um input do tipo texto dentro de um form, foi usado estado para guardar o que fosse digitado nesse input

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,43 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputTextValue: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ inputTextValue: event.target.value });
+  }
+
+  renderInput() {
+    const { inputTextValue } = this.state;
+    return (
+      <input
+        type="text"
+        data-testid="title-input"
+        id="title-input"
+        value={ inputTextValue }
+        onChange={ this.handleChange }
+      />
+    );
+  }
+
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          { this.renderInput() }
+        </label>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <AddMovie />
       </div>
     );
   }
