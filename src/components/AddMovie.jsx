cf582f6b9d5621dd64c3fd49537f61608011d9bd
Humberto Bandeira <humberto-bandeira@hotmail.com>
2021-01-18 21:02:58 -0400

    Testando Requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,11 +3,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+    this.handleState = this.handleState.bind(this);
+  }
+
+  handleState(event) {
+    const { value, name } = event.target;
+    this.setState(() => ({ [name]: value,
+    }));
+  }
+
+  insertInput(refState, handleState, testeID, name) {
+    return (
+      <label htmlFor="inputInsert" data-testid="title-input-label">
+        Título
+        <input
+          onChange={ handleState }
+          value={ refState }
+          name={ name }
+          id="inputInsert"
+          data-testid={ testeID }
+        />
+      </label>
+    );
+  }
+
   render() {
     const { onClick } = this.props;
+    const { title, subtitle } = this.state;
     return (
       <form data-testid="add-movie-form">
         <button type="button" onClick={ onClick }>eu aqui</button>
+        {this.insertInput(title, this.handleState, 'title-input', 'title')}
+        {this.insertInput(subtitle, this.handleState, 'title-input', 'subtitle')}
       </form>
     );
   }
