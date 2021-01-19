caeac47f1db3c4513931fc4a034c886c10c45aa2
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-19 09:40:30 -0300

    Requisito 16 concluido

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,23 @@
 // implement MovieLibrary component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+import SearchBar from './SearchBar';
 
 class MovieLibrary extends Component {
   render() {
-    return <p>Movie Library</p>;
+    const { movies } = this.props;
+    return (
+      <div>
+        <SearchBar searchText="" bookmarkedOnly={ false } selectedGenre="" />
+        { movies.map((movie, index) => <MovieCard key={ index } movie={ movie } />) }
+      </div>
+    );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieLibrary;
