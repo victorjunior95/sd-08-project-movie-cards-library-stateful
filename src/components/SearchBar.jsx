41cbe765b7ffd2c2b6f0f37649d1f98c4b947b87
wesleypribeiro <wesley_paulo00@hotmail.com>
2021-01-19 15:28:41 -0300

    R03 - Concluido

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  render() {
+  renderCheckox() {
+    const { bookmarkedOnly, onBookmarkChange } = this.props;
     return (
-      <form data-testid="search-bar-form" />
+      <label htmlFor="Alguma" data-testid="checkbox-input-label">
+        <input
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkChange }
+          data-testid="checkbox-input-label"
+        />
+        Mostrar somente favoritos
+      </label>
+    );
+  }
+
+  render() {
+    const { searchText, onSearchTextChange } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto
+          <input
+            type="text"
+            id="teste"
+            data-testid="text-input"
+            value={ searchText }
+            onChange={ onSearchTextChange }
+          />
+        </label>
+        {this.renderCheckox()}
+      </form>
     );
   }
 }
