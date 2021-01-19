e00a38fa0467c214844ff48668056da56cf1f5df
Silvio Dayube <scarige@gmail.com>
2021-01-18 21:24:42 -0300

    Req 5 - Render a select inside the search form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -34,11 +34,33 @@ class SearchBar extends React.Component {
     );
   }
 
+  renderSelect() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+
+    return (
+      <label data-testid="select-input-label" htmlFor="select">
+        Filtrar por gênero
+        <select
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+          id="select"
+          data-testid="select-input"
+        >
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     return (
       <form data-testid="search-bar-form">
         {this.renderSearchInput()}
         {this.renderCheckbox()}
+        {this.renderSelect()}
       </form>
     );
   }
