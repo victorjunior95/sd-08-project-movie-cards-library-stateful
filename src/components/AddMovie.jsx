0a9e521d9f0045a4a810e6984a1aa4d9f4b5f606
Silvio Dayube <scarige@gmail.com>
2021-01-18 22:16:06 -0300

    Req 9 - Render subtitle input within <AddMovie />

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -25,7 +25,6 @@ class AddMovie extends React.Component {
 
   renderTitle() {
     const { title } = this.state;
-
     return (
       <label data-testid="title-input-label" htmlFor="titleInput">
         Título
@@ -40,11 +39,28 @@ class AddMovie extends React.Component {
     );
   }
 
+  renderSubtitle() {
+    const { subtitle } = this.state;
+    return (
+      <label data-testid="subtitle-input-label" htmlFor="subtitleInput">
+        Subtítulo
+        <input
+          id="subtitleInput"
+          data-testid="subtitle-input"
+          name="subtitle"
+          value={ subtitle }
+          onChange={ this.handleInputChange }
+        />
+      </label>
+    );
+  }
+
   render() {
     const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
         { this.renderTitle() }
+        { this.renderSubtitle() }
       </form>
     );
   }
