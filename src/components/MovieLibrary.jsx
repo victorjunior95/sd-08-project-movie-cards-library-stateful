c115a09ec23e072e00e9aa037a55f3edfbe1184c
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-01-19 15:10:18 -0300

    adiciona filtros

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
-// import MovieList from './MovieList';
+import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
 
     this.state = {
-      searchText: 'a',
+      searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      // movies: this.props.movies,
+      movies: props.movies,
     };
     console.log(this);
   }
@@ -39,8 +39,22 @@ class MovieLibrary extends React.Component {
     this.setState(state);
   }
 
+  filter(array, searchText, selectedGenre, bookmarkedOnly) {
+    return (
+      array.filter((elem) => {
+        if (bookmarkedOnly === true) {
+          return elem.bookmarked === true;
+        } return elem;
+      }).filter((a) => {
+        if (selectedGenre === '') {
+          return a;
+        } return a.genre === selectedGenre;
+      }).filter((name) => (name.title.toUpperCase().includes(searchText.toUpperCase())))
+    );
+  }
+
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
 
     return (
       <div>
@@ -53,14 +67,15 @@ class MovieLibrary extends React.Component {
           onSelectedGenreChange={ this.onSelectedGenreChange }
         />
         <AddMovie onClick={ this.callbackParaAddmovie } />
-        {/* <MovieList
-        movies={ movies.filter((elem) => elem.value.contains(searchText)) } /> */}
+        <MovieList
+          movies={ this.filter(movies, searchText, selectedGenre, bookmarkedOnly) }
+        />
       </div>
     );
   }
 }
-// MovieLibrary.propTypes = {
-//   movies: PropTypes.arrayOf().isRequired,
-// };
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf().isRequired,
+};
 
 export default MovieLibrary;
