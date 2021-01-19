80b67599ea8cdf25ecd38ca3820dd9ad3af9b0b2
Aline Debastiani <debastianialine@gmail.com>
2021-01-19 15:59:11 -0300

    cumpre exercicios 1 a 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,86 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
+  constructor() {
+    super();
+    this.textInput = this.textInput.bind(this);
+    this.checkBoxInput = this.checkBoxInput.bind(this);
+    this.selectedGenreInput = this.selectedGenreInput.bind(this);
+  }
+
+  textInput() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <label data-testid="text-input-label" htmlFor="text-input">
+        Inclui o texto:
+        <input
+          type="text"
+          name="searchText"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+        />
+      </label>
+    );
+  }
+
+  checkBoxInput() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    return (
+      <label data-testid="checkbox-input-label" htmlFor="checkBoxInput">
+        Mostrar somente favoritos
+        <input
+          type="checkbox"
+          name="bookmarkedOnly"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+        />
+      </label>
+    );
+  }
+
+  selectedGenreInput(options) {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label data-testid="select-input-label" htmlFor="selectedGenreInput">
+        Filtrar por gênero
+        <select
+          name="selectedGenre"
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+        >
+          {options.map(({ text, value }) => (
+            <option key={ text } data-testid="select-option" value={ value }>
+              { text }
+            </option>
+          ))}
+        </select>
+      </label>
+    );
+  }
+
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
-      selectedGenre, onSelectedGenreChange } = this.props;   
+    const options = [
+      { text: 'Todos', value: '' },
+      { text: 'Ação', value: 'action' },
+      { text: 'Comédia', value: 'comedy' },
+      { text: 'Suspense', value: 'thriller' },
+    ];
+    return (
+      <form data-testid="search-bar-form">
+        {this.textInput()}
+        {this.checkBoxInput()}
+        {this.selectedGenreInput(options)}
+      </form>
+    );
   }
 }
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
-  bookmarkedOnly: PropTypes.boolean.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
   onBookmarkedChange: PropTypes.func.isRequired,
   selectedGenre: PropTypes.string.isRequired,
   onSelectedGenreChange: PropTypes.func.isRequired,
