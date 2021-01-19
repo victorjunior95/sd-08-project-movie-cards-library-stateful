3244a4f19994163ab4176aaf1b4eccdc1b3a8a29
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-01-19 16:34:25 -0300

    Req1: finalizado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import SearchBar from './components/SearchBar'
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,35 @@
-// implement SearchBar component here
+import React, { Component } from 'react'
+
+
+export default class SearchBar extends Component {
+    
+    testeFuncao (searchText, onSearchTextChange) {
+        return (
+            <label data-testid="text-input-label">
+                  Inclui o texto:
+                   <input onChange={onSearchTextChange} value={searchText} data-testid="text-input" type="text"/>
+               </label>
+        );
+    }
+    render() {
+        const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+        return (
+           <form data-testid="search-bar-form">
+               {this.testeFuncao(searchText, onSearchTextChange)}
+               <label data-testid="checkbox-input-label">
+                   Mostrar somente favoritos
+                    <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} data-testid="checkbox-input"/>
+               </label>
+               <label data-testid="select-input-label">
+                    Filtrar por gênero
+                   <select value={selectedGenre} onChange={onSelectedGenreChange} data-testid="select-input">
+                       <option data-testid="select-option" value="">Todos</option>
+                       <option data-testid="select-option" value="action">Ação</option>
+                       <option data-testid="select-option" value="comedy">Comédia</option>
+                       <option data-testid="select-option" value="thriller">Suspense</option>
+                   </select>
+               </label>
+           </form>
+        );
+    }
+}
