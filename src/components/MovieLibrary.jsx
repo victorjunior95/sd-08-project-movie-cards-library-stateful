e450bd11679f0a0eb08eae0996fa5baa3ae74c02
victor Farias <victorfarias.tv@gmail.com>
2021-01-19 12:45:39 -0300

    Adicionado requisitos finais

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,75 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: '',
+      selectedGenre: '',
+      movies,
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+    this.handleSearchEvent = this.handleSearchEvent.bind(this);
+    this.handleAddMovie = this.handleAddMovie.bind(this);
+  }
+
+  handleSearchEvent({ target }) {
+    this.setState({
+      [target.name]: target.type === 'checkbox' ? target.checked : target.value,
+    });
+  }
+
+  handleAddMovie(objectMovie) {
+    this.setState((currentState) => ({
+      movies: [...currentState.movies, objectMovie],
+    }));
+  }
+
+  handleFilter(movies) {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    let moviesToShow = movies
+      .filter((movie) => movie.genre.includes(selectedGenre))
+      .filter((movie) => movie.title.includes(searchText)
+        || movie.subtitle.includes(searchText)
+        || movie.storyline.includes(searchText));
+    moviesToShow = !bookmarkedOnly
+      ? moviesToShow
+      : moviesToShow.filter((movie) => movie.bookmarked);
+
+    return moviesToShow;
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, movies, selectedGenre } = this.state;
+    const showMovies = this.handleFilter(movies);
+    return (
+      <main>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+          onSearchTextChange={ this.handleSearchEvent }
+          onBookmarkedChange={ this.handleSearchEvent }
+          onSelectedGenreChange={ this.handleSearchEvent }
+        />
+        <MovieList movies={ showMovies } />
+        <AddMovie onClick={ this.handleAddMovie } />
+      </main>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,6 +9,7 @@ class SearchBar extends React.Component {
       <label data-testid="text-input-label" htmlFor="imput-text">
         Inclui o texto:
         <input
+          name="searchText"
           data-testid="text-input"
           type="text"
           value={ searchText }
@@ -25,6 +26,7 @@ class SearchBar extends React.Component {
       <label data-testid="checkbox-input-label" htmlFor="input-checkbox">
         Mostrar somente favoritos
         <input
+          name="bookmarkedOnly"
           data-testid="checkbox-input"
           type="checkbox"
           checked={ bookmarkedOnly }
@@ -41,6 +43,7 @@ class SearchBar extends React.Component {
       <label data-testid="select-input-label" htmlFor="select-input">
         Filtrar por gênero
         <select
+          name="selectedGenre"
           data-testid="select-input"
           value={ selectedGenre }
           onChange={ onSelectedGenreChange }
