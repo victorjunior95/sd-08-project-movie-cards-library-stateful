361176377baa1ed3bdec2a5c1cfef6940de24cbe
Humberto Bandeira <humberto-bandeira@hotmail.com>
2021-01-18 22:51:09 -0400

    Requisitos 12 a 13

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,6 +11,7 @@ class AddMovie extends React.Component {
       imagePath: '',
       storyline: '',
       rating: 0,
+      genre: '',
     };
     this.handleState = this.handleState.bind(this);
   }
@@ -73,10 +74,29 @@ class AddMovie extends React.Component {
     );
   }
 
+  renderSelect(refState, name, content, handleState) {
+    return (
+      <label htmlFor={ name } data-testid={ `${name}-input-label` }>
+        { content }
+        <select
+          onChange={ handleState }
+          value={ refState }
+          name={ name }
+          data-testid={ `${name}-input` }
+        >
+          <option value="action" data-testid={ `${name}-option` }>Ação</option>
+          <option value="comedy" data-testid={ `${name}-option` }>Comédia</option>
+          <option value="thriller" data-testid={ `${name}-option` }>Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
-    const { insertInputText, insertTextArea, handleState, insertInputNumber } = this;
+    const { insertInputText, insertTextArea, handleState, insertInputNumber,
+      renderSelect } = this;
     const { onClick } = this.props;
-    const { title, subtitle, imagePath, storyline, rating } = this.state;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
         <button type="button" onClick={ onClick }>eu aqui</button>
@@ -85,6 +105,7 @@ class AddMovie extends React.Component {
         {insertInputText(imagePath, 'image', 'Imagem', handleState)}
         {insertTextArea(storyline, 'storyline', 'Sinopse', handleState)}
         {insertInputNumber(rating, 'rating', 'Avaliação', handleState)}
+        {renderSelect(genre, 'genre', 'Gênero', handleState)}
       </form>
     );
   }
