7d02a99c96d5cac9fb56b9f9ffaeeb721de4deec
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-19 16:23:25 -0300

    Req06: creates an AddMovie component and it renders

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super()
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+
+  render() {
+    return(
+      null
+    )
+  }
+}
+
+export default AddMovie;
