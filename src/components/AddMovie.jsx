14ba31bbcadceffe86c8e3edf016bbbfa271b172
wend3ll-souza <wend3llsouza@gmail.com>
2021-01-19 19:59:54 -0300

    :package: Requisito 7 Pronto

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+        subtitle: '',
+        title: '',
+        imagePath: '',
+        storyline: '',
+        rating: 0,
+        genre: 'action'
+      }
+  }
   render() {
     return (
-      <form action="">
-    
-      </form>
+      <form data-testid="add-movie-form" ></form>
     );
   }
 }
