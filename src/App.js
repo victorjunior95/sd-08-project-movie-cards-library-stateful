8de71f39e22a09c4ad2c2a8c78ee3329eea7e36a
emassaki <massakihirayama@gmail.com>
2021-01-18 22:32:24 -0300

    Finalizado segundo componente - AddMovie (requisitos 6 a 14)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,89 @@
 import React from 'react';
-import './App.css';
 
+import movies from './data';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import SearchBar from './components/SearchBar';
-import movies from './data';
+import AddMovie from './components/AddMovie';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <SearchBar />
-      <MovieList movies={ movies } />
-    </div>
-  );
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: 'Todos',
+    };
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.setSearchBar = this.setSearchBar.bind(this);
+  }
+
+  onSearchTextChange({ target }) {
+    this.setState({
+      searchText: target.value,
+    });
+  }
+
+  onBookmarkedChange({ target }) {
+    this.setState({
+      bookmarkedOnly: target.checked,
+    });
+  }
+
+  onSelectedGenreChange({ target }) {
+    this.setState({
+      selectedGenre: target.value,
+    });
+  }
+
+  onButtonClick() {
+    console.log(this.state);
+  }
+
+  setSearchBar() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+
+    return (
+      <SearchBar
+        searchText={ searchText }
+        onSearchTextChange={ this.onSearchTextChange }
+        bookmarkedOnly={ bookmarkedOnly }
+        onBookmarkedChange={ this.onBookmarkedChange }
+        selectedGenre={ selectedGenre }
+        onSelectedGenreChange={ this.onSelectedGenreChange }
+      />
+    );
+  }
+
+  setMovieList() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+
+    return (
+      <MovieList
+        movies={ movies }
+        searchText={ searchText }
+        bookmarkedOnly={ bookmarkedOnly }
+        selectedGenre={ selectedGenre }
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        {this.setSearchBar()}
+        {this.setMovieList()}
+        <AddMovie onClick={ this.onButtonClick } />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,132 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    let { value } = target;
+    if (name === 'rating') value = parseFloat(value);
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick(callback) {
+    callback(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  renderInput(text, name, value, handleChange) {
+    const testName = name === 'imagePath' ? 'image' : name;
+    const type = name === 'rating' ? 'number' : 'text';
     return (
-      <div>Oi</div>
+      <label htmlFor={ name } data-testid={ `${testName}-input-label` }>
+        {text}
+        <input
+          type={ type }
+          name={ name }
+          id={ name }
+          value={ value }
+          onChange={ handleChange }
+          data-testid={ `${testName}-input` }
+          step={ 0.1 }
+          max={ 5 }
+          min={ 0 }
+        />
+      </label>
+    );
+  }
+
+  renderTextarea(name, handleChange) {
+    return (
+      <label htmlFor="storyLine" data-testid="storyline-input-label">
+        Sinopse
+        <textarea
+          name="storyLine"
+          id="storyLine"
+          value={ name }
+          onChange={ handleChange }
+          cols="30"
+          rows="10"
+          data-testid="storyline-input"
+        />
+      </label>
+    );
+  }
+
+  renderSelect(name, handleChange) {
+    return (
+      <label htmlFor="genre" data-testid="genre-input-label">
+        Gênero
+        <select
+          name="genre"
+          id="genre"
+          value={ name }
+          onChange={ handleChange }
+          data-testid="genre-input"
+        >
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  renderButton(onClick) {
+    return (
+      <button
+        type="button"
+        onClick={ () => this.handleClick(onClick) }
+        data-testid="send-button"
+      >
+        Adicionar filme
+      </button>
+    );
+  }
+
+  render() {
+    const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyLine, rating, genre } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        {this.renderInput('Título', 'title', title, this.handleChange)}
+        {this.renderInput('Subtítulo', 'subtitle', subtitle, this.handleChange)}
+        {this.renderInput('Imagem', 'imagePath', imagePath, this.handleChange)}
+        {this.renderTextarea(storyLine, this.handleChange)}
+        {this.renderInput('Avaliação', 'rating', rating, this.handleChange)}
+        {this.renderSelect(genre, this.handleChange)}
+        {this.renderButton(onClick)}
+      </form>
     );
   }
 }
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default AddMovie;
