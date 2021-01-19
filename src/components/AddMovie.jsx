dea635bdda5e9965d9eb823295daa1d633a923c0
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-01-19 10:28:20 -0300

    adicionando requisito 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,21 +2,19 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      subtitle: '',
-      title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
-    };
-  }
-
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     subtitle: '',
+  //     title: '',
+  //     imagePath: '',
+  //     storyline: '',
+  //     rating: 0,
+  //     genre: 'action',
+  //   };
+  // }
   render() {
-    return <div>a</div>;
+    return <form data-testid="add-movie-form" />;
   }
 }
 
