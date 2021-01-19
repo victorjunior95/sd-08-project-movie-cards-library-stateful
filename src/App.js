cc6eb92d03cca1df468cd234d7637124fa9d63c2
Seu nome <luiza.dilly@gmail.com>
2021-01-19 17:00:19 -0300

    adiciona os cards dos filmes renderizados

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 
 import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
 // import MovieCard from './components/MovieCard';
-// import movies from './data';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
