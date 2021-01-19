45c31294f3d71ef5f6382a9171d5a373e53f0e9b
enicio <jessandro42@gmail.com>
2021-01-19 20:20:17 -0300

    Nono requisito

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  inputTitle() {
+    return (
+      <label data-testid="title-input-label" htmlFor="title">
+        Título
+        <input data-testid="title-input" id="title" type="text" />
+      </label>
+    );
+  }
+
+  inputSubtitle() {
+    return (
+      <label data-testid="subtitle-input-label" htmlFor="subtitle">
+        Subtítulo
+        <input data-testid="subtitle-input" id="subtitle" type="text" />
+      </label>
+    );
+  }
+
   render() {
     return (
       <div>
         <form data-testid="add-movie-form" action="">
-          <label data-testid="title-input-label" htmlFor="title">
-            Título
-            <input data-testid="title-input" id="title" type="text" />
-          </label>
+          {this.inputTitle()}
+          {this.inputSubtitle()}
         </form>
       </div>
     );
