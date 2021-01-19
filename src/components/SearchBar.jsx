4f56986e59dc33f5362a1851d5972bffe16e55a9
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-01-19 09:45:09 -0300

    adicionando requisito 4

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 import InputField from './InputField';
 
 class SearchBar extends React.Component {
+  renderCheckBoxField(props) {
+    return (
+      <label htmlFor="checkbox" data-testid="checkbox-input-label">
+        Mostrar somente favoritos
+        <input
+          type="checkbox"
+          name="checkbox"
+          checked={ props.bookmarkedOnly }
+          onChange={ props.onBookmarkedChange }
+          data-testid="checkbox-input"
+        />
+      </label>
+    );
+  }
+
   render() {
     const objProps = this.props;
     return (
       <form data-testid="search-bar-form">
         <InputField objProps={ objProps } />
+        { this.renderCheckBoxField(objProps) }
       </form>
     );
   }
