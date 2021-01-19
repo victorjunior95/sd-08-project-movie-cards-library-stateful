8427bac6f53f4134faa057479d3e3a01cd6e57c9
Silvio Dayube <scarige@gmail.com>
2021-01-19 12:06:31 -0300

    Req 20 - Adjusts remaining proptypes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -16,14 +16,14 @@ class MovieList extends React.Component {
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.shape({
+  movies: PropTypes.arrayOf(PropTypes.shape({
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     rating: PropTypes.number.isRequired,
     imagePath: PropTypes.string.isRequired,
-    map: PropTypes.func.isRequired,
-  }).isRequired,
+    map: PropTypes.func,
+  })).isRequired,
 };
 
 export default MovieList;
