faf54ad9a0d53e5319f266c8665ba4c6d9fdef9f
Fernanda Marques <nandateixmarques@gmail.com>
2021-01-19 16:57:57 -0300

    adicionando o requisito 3 - Renderize um input do tipo texto dentro do formulário em <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
-      <form data-testid="search-bar-form" />
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="IDinput">
+          Inclui o texto:
+          <input type="text" id="IDinput" value={ searchText }
+            onChange={ onSearchTextChange } data-testid="text-input" />
+        </label>
+      </form>
     );
   }
 }
