eb8ce8761761fb33878565fd61c8e05c6dc9de68
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-01-19 15:50:55 -0300

    adicionando requisito 16

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import AddMovie from './components/AddMovie';
+// import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 import Header from './components/Header';
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <AddMovie />
+      <MovieLibrary />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,41 @@
 // implement MovieLibrary component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+  }
+
   render() {
-    return <p>asdf</p>;
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    console.log(searchText, bookmarkedOnly, selectedGenre);
+    return (
+      <div>
+        <SearchBar />
+        <MovieList movies={ movies } />
+      </div>
+    );
   }
 }
 
 export default MovieLibrary;
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
