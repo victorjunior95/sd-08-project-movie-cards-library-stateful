5a5cdfec61538e58e96dfd1ab6dcd4c505220a23
enicio <jessandro42@gmail.com>
2021-01-19 19:41:36 -0300

    Oitavo requisito

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,7 +4,12 @@ class AddMovie extends React.Component {
   render() {
     return (
       <div>
-        <form data-testid="add-movie-form" action="" />
+        <form data-testid="add-movie-form" action="">
+          <label data-testid="title-input-label" htmlFor="title">
+            Título
+            <input data-testid="title-input" id="title" type="text" />
+          </label>
+        </form>
       </div>
     );
   }
