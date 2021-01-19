cea2308205d94e5adbcb6b5e1bdffafed5b2f8ab
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-01-19 20:41:11 -0300

    testando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+<<<<<<< HEAD
 import './App.css';
 import MovieLibrary from './components/MovieLibrary';
+=======
+>>>>>>> ed9b3735b0db47e753a25f535b15b29721bafcbe
 import Header from './components/Header';
+import './App.css';
 
 function App() {
   return (
