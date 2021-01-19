910125f0aa373376b973094dfc1efcfeabb1633b
Fernanda Marques <nandateixmarques@gmail.com>
2021-01-19 18:17:28 -0300

    adicionando o requisito 5 - Renderize um select dentro do formulário em <SearchBar />

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,32 +2,64 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
+  renderTextInput() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <label data-testid="text-input-label" htmlFor="IDinput">
+        Inclui o texto:
+        <input
+          type="text"
+          id="IDinput"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+        />
+      </label>
+    );
+  }
+
+  renderCheckBox() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    return (
+      <label data-testid="checkbox-input-label" htmlFor="IDChecked">
+        Mostrar somente favoritos
+        <input
+          type="checkbox"
+          id="IDChecked"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+        />
+      </label>
+    );
+  }
+
+  renderSelect() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label data-testid="select-input-label" htmlFor="IDSelect">
+        Filtrar por gênero
+        <select
+          id="IDSelect"
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+          data-testid="select-input"
+        >
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly,
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor="IDinput">
-          Inclui o texto:
-          <input type="text" id="IDinput" value={ searchText }
-            onChange={ onSearchTextChange } data-testid="text-input"
-          />
-        </label>
-        <label data-testid="checkbox-input-label" htmlFor="IDChecked">
-          Mostrar somente favoritos
-          <input type="checkbox" id="IDChecked" checked={ bookmarkedOnly }
-            onChange={ onBookmarkedChange } data-testid="checkbox-input"
-          />
-        </label>
-        <label data-testid="select-input-label">
-          Filtrar por gênero
-          <select value={ selectedGenre } onChange={ onSelectedGenreChange } data-testid="select-input">
-            <option value="" data-testid="select-option">Todos</option>
-            <option value="action" data-testid="select-option">Ação</option>
-            <option value="comedy" data-testid="select-option">Comédia</option>
-            <option value="thriller" data-testid="select-option">Suspense</option>
-          </select>
-        </label>
+        { this.renderTextInput() }
+        { this.renderCheckBox() }
+        { this.renderSelect() }
       </form>
     );
   }
