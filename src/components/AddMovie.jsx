c6aa4c2a7ed169d04c046e59a412f362488e42a7
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-01-19 11:17:16 -0300

    Implementado uma textarea dentro do formulário do AddMovie para obter o caminho a sinopse do novo filme. Render do componente AddMovie foi refatorado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -8,6 +8,7 @@ class AddMovie extends React.Component {
       inputTextTitle: '',
       inputTextSubTitle: '',
       inputUrl: '',
+      inputTextStoryline: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -22,60 +23,77 @@ class AddMovie extends React.Component {
   renderInputTitle() {
     const { inputTextTitle } = this.state;
     return (
-      <input
-        name="inputTextTitle"
-        type="text"
-        data-testid="title-input"
-        id="title-input"
-        value={ inputTextTitle }
-        onChange={ this.handleChange }
-      />
+      <label data-testid="title-input-label" htmlFor="title-input">
+        Título
+        <input
+          name="inputTextTitle"
+          type="text"
+          data-testid="title-input"
+          id="title-input"
+          value={ inputTextTitle }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  renderInputStoryline() {
+    const { inputTextStoryline } = this.state;
+    return (
+      <label data-testid="storyline-input-label" htmlFor="storyline-input">
+        Sinopse
+        <textarea
+          name="inputTextStoryline"
+          data-testid="storyline-input"
+          id="storyline-input"
+          value={ inputTextStoryline }
+          onChange={ this.handleChange }
+        />
+      </label>
     );
   }
 
   renderInputSubTitle() {
     const { inputTextSubTitle } = this.state;
     return (
-      <input
-        name="inputTextSubTitle"
-        type="text"
-        data-testid="subtitle-input"
-        id="subtitle-input"
-        value={ inputTextSubTitle }
-        onChange={ this.handleChange }
-      />
+      <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
+        Subtítulo
+        <input
+          name="inputTextSubTitle"
+          type="text"
+          data-testid="subtitle-input"
+          id="subtitle-input"
+          value={ inputTextSubTitle }
+          onChange={ this.handleChange }
+        />
+      </label>
     );
   }
 
   renderInputImagePath() {
     const { inputUrl } = this.state;
     return (
-      <input
-        name="inputUrl"
-        type="text"
-        data-testid="image-input"
-        id="image-input"
-        value={ inputUrl }
-        onChange={ this.handleChange }
-      />
+      <label data-testid="image-input-label" htmlFor="image-input">
+        Imagem
+        <input
+          name="inputUrl"
+          type="text"
+          data-testid="image-input"
+          id="image-input"
+          value={ inputUrl }
+          onChange={ this.handleChange }
+        />
+      </label>
     );
   }
 
   render() {
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="title-input">
-          Título
-          { this.renderInputTitle() }
-        </label>
-        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
-          Subtítulo
-          { this.renderInputSubTitle() }
-        </label>
-        <label data-testid="image-input-label" htmlFor="image-input">
-          Imagem
-          { this.renderInputImagePath() }
-        </label>
+        { this.renderInputTitle() }
+        { this.renderInputSubTitle() }
+        { this.renderInputImagePath() }
+        { this.renderInputStoryline() }
       </form>
     );
   }
