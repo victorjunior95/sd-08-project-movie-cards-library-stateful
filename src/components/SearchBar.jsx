1394311cd3a86240aeb10c4f6df992702a18bc11
Ronald Jr <rpintojunior@gmail.com>
2021-01-19 17:06:24 -0300

    Insere Select

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
-  // eslint-disable-next-line max-lines-per-function
   renderTextInput() {
     const {
       searchText,
@@ -17,6 +16,27 @@ class SearchBar extends React.Component {
           onChange={ onSearchTextChange }
         />
       </label>);
+  }
+
+  renderSelect() {
+    const { selectedGenre,
+      onSelectedGenreChange } = this.props;
+    return (
+      <label htmlFor="select-input" data-testid="select-input-label">
+        Filtrar por gênero
+        <select
+          name="selectedGenre"
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+          data-testid="select-input"
+        >
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </label>
+    );
   }
 
   renderCheckBox() {
@@ -40,6 +60,7 @@ class SearchBar extends React.Component {
       <form data-testid="search-bar-form">
         { this.renderTextInput() }
         { this.renderCheckBox() }
+        {this.renderSelect()}
       </form>
     );
   }
@@ -49,6 +70,8 @@ SearchBar.propTypes = {
   onSearchTextChange: PropTypes.func.isRequired,
   bookmarkedOnly: PropTypes.bool.isRequired,
   onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
