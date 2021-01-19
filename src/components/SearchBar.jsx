8e5ab724deebc49d627ad68af79d173f99205e19
Leonardo <lecastro16@gmail.com>
2021-01-19 09:56:20 -0300

    finalizando a searchBar v0

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,6 +6,54 @@ class SearchBar extends React.Component {
   //     super();
   //   }
 
+  fillTextLabelHmlt(searchText, onSearchTextChange) {
+    return (
+      <label htmlFor="text" data-testid="text-input-label">
+        Inclui o texto:
+        <input
+          type="text"
+          name="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+        />
+      </label>
+    );
+  }
+
+  fillCheckboxHtml(bookmarkedOnly, onBookmarkedChange) {
+    return (
+      <label htmlFor="checkbox" data-testid="checkbox-input-label">
+        Mostrar somente favoritos
+        <input
+          data-testid="checkbox-input"
+          type="checkbox"
+          name="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+        />
+      </label>
+    );
+  }
+
+  fillSelectHtml(selectedGenre, onSelectedGenreChange) {
+    return (
+      <label htmlFor="selectList" data-testid="select-input-label">
+        Filtrar por gênero
+        <select
+          name="selectList"
+          data-testid="select-input"
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+        >
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     const {
       searchText,
@@ -17,47 +65,9 @@ class SearchBar extends React.Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="text" data-testid="text-input-label">
-          Inclui o texto:
-          <input
-            type="text"
-            name="text"
-            value={ searchText }
-            onChange={ onSearchTextChange }
-          />
-        </label>
-        <label htmlFor="checkbox" data-testid="checkbox-input-label">
-          Mostrar somente favoritos
-          <input
-            data-testid="checkbox-input"
-            type="checkbox"
-            name="checkbox"
-            checked={ bookmarkedOnly }
-            onChange={ onBookmarkedChange }
-          />
-        </label>
-        <label htmlFor="selectList" data-testid="select-input-label">
-          Filtrar por gênero
-          <select
-            name="selectList"
-            data-testid="select-input"
-            value={ selectedGenre }
-            onChange={ onSelectedGenreChange }
-          >
-            <option data-testid="select-option" value="">
-              Todos
-            </option>
-            <option data-testid="select-option" value="action">
-              Ação
-            </option>
-            <option data-testid="select-option" value="comedy">
-              Comédia
-            </option>
-            <option data-testid="select-option" value="thriller">
-              Suspense
-            </option>
-          </select>
-        </label>
+        {this.fillTextLabelHmlt(searchText, onSearchTextChange)}
+        {this.fillCheckboxHtml(bookmarkedOnly, onBookmarkedChange)}
+        {this.fillSelectHtml(selectedGenre, onSelectedGenreChange)}
       </form>
     );
   }
