8cabed04a59a2c228e78351e684d2689763e9c6a
Leonardo <lecastro16@gmail.com>
2021-01-19 18:13:31 -0300

    fazendo mais um update, varios requisitos v5

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -128,7 +128,7 @@ class AddMovie extends React.Component {
           onChange={ onSelectedGenreChange }
         >
           <option data-testid="genre-option" value="action">Ação</option>
-          <option data-testid="genre-option" value="comedy">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
           <option data-testid="genre-option" value="thriller">Suspense</option>
         </select>
       </label>
