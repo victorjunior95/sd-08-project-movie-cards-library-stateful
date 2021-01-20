70dbf5b40619f40ee7716010941edc413987bdee
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-01-19 22:11:22 -0300

    Req 17, OK

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -16,14 +16,36 @@ class MovieLibrary extends React.Component {
       selectedGenre: '',
       movies: [movies],
     };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
   }
 
   onClick(data) {
     console.log(data);
   }
 
+  onSearchTextChange(event) {
+    const { searchText } = this.state;
+    this.setState({ searchText: event.target.value });
+    console.log(searchText);
+  }
+
+  onBookmarkedChange() {
+    const { bookmarkedOnly } = this.state;
+    this.setState({ bookmarkedOnly: true });
+    console.log(bookmarkedOnly);
+  }
+
+  onSelectedGenreChange(select) {
+    const { selectedGenre } = this.state;
+    this.setState({ selectedGenre: select.target.value });
+    console.log(selectedGenre);
+  }
+
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const { onSearchTextChange, onBookmarkedChange, onSelectedGenreChange } = this;
     return (
       <div>
         <h2> My awesome movie library </h2>
@@ -31,8 +53,11 @@ class MovieLibrary extends React.Component {
           searchText={ searchText }
           bookmarkedOnly={ bookmarkedOnly }
           selectedGenre={ selectedGenre }
+          onSearchTextChange={ onSearchTextChange }
+          onBookmarkedChange={ onBookmarkedChange }
+          onSelectedGenreChange={ onSelectedGenreChange }
         />
-        <MovieList movies={ movies.find((e) => e[0]) } />
+        <MovieList movies={ movies.find((array) => array[0]) } />
         <AddMovie onClick={ this.onClick } />
       </div>
     );
