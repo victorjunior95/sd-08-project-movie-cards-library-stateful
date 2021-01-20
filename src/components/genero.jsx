18051ff79b63dde227cc7092ae76ad881bd2fcce
Clênio Galvão <clenio.cgm@gmail.com>
2021-01-20 15:55:55 -0300

    resolve 19

--- src/components/genero.jsx
+++ src/components/genero.jsx
@@ -4,28 +4,28 @@ import PropTypes from 'prop-types';
 
 class Genero extends React.Component {
   render() {
-    const { onChange } = this.props;
+    const { value, onChange } = this.props;
     return (
-      <div>
-        <label data-testid="genre-input-label" htmlFor="gen">
-          Gênero
-          <select
-            id="gen"
-            name="genre"
-            data-testid="genre-input"
-            onChange={ onChange }
-          >
-            <option value="action" data-testid="genre-option">Ação</option>
-            <option value="comedy" data-testid="genre-option">Comédia</option>
-            <option value="thriller" data-testid="genre-option">Suspense</option>
-          </select>
-        </label>
-      </div>
+      <label data-testid="genre-input-label" htmlFor="gen">
+        Gênero
+        <select
+          id="gen"
+          value={ value }
+          name="genre"
+          data-testid="genre-input"
+          onChange={ onChange }
+        >
+          <option value="action" data-testid="genre-option">Ação</option>
+          <option value="comedy" data-testid="genre-option">Comédia</option>
+          <option value="thriller" data-testid="genre-option">Suspense</option>
+        </select>
+      </label>
     );
   }
 }
 
 Genero.propTypes = {
+  value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
 
