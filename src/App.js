2e7dc3e6edf5058177d79de912c4231074a547fb
Tiago <tiagaoalb@gmail.com>
2021-01-20 16:24:16 -0300

    first try

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar search={ movies } />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,70 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  onSearch(param1, param2) {
+    return (
+      <label id="text-input-label" htmlFor="text-input">
+        Inclui o texto
+        <input id="text-input" type="text" onChange={ param1 } />
+        { param2 }
+      </label>
+    );
+  }
+
+  onBookMark(param1, param2) {
+    return (
+      <label id="checkbox-input-label" htmlFor="checkbox-input">
+        Mostrar somente favoritos
+        <input
+          id="checkbox-input"
+          type="checkbox"
+          onChange={ param1 }
+          checked={ param2 }
+        />
+      </label>
+    );
+  }
+
+  select(param1, param2) {
+    return (
+      <label htmlFor="select-input">
+        Filtrar por gênero
+        <select name="" id="select-input" value={ param1 } onChange={ param2 }>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  render() {
+    const { search } = this.props;
+    const { searchText, onSearchTextChange,
+      bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange,
+    } = search;
+    return (
+      <form id="search-bar-form">
+        { this.onSearch(onSearchTextChange, searchText) }
+        { this.onBookMark(onBookmarkedChange, bookmarkedOnly) }
+        { this.select(selectedGenre, onSelectedGenreChange) }
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  search: PropTypes.shape({
+    searchText: PropTypes.string.isRequired,
+    onSearchTextChange: PropTypes.func.isRequired,
+    bookmarkedOnly: PropTypes.bool.isRequired,
+    onBookmarkedChange: PropTypes.func.isRequired,
+    selectedGenre: PropTypes.string.isRequired,
+    onSelectedGenreChange: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default SearchBar;
