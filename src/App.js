7ee6aae3c1208158cee7f16f19d21b60dcdb3310
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-01-20 07:41:27 -0300

    ate req 17 corrigido

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
-import MovieLibrary from './components/MovieLibrary'
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
