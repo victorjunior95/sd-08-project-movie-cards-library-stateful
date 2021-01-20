4088372b249aa1c7ac03df95be9dc90865c7ba7c
enicio <jessandro42@gmail.com>
2021-01-19 22:11:31 -0300

    Decimo terceiro requisito

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -52,6 +52,20 @@ class AddMovie extends React.Component {
     );
   }
 
+  inputSelect() {
+    return (
+      <label data-testid="genre-input-label" htmlFor="gender">
+        Gênero
+        <select data-testid="genre-input" name="gender" id="gender">
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+
+    );
+  }
+
   render() {
     return (
       <div>
@@ -61,6 +75,7 @@ class AddMovie extends React.Component {
           {this.inputImage()}
           {this.inputSinopse()}
           {this.inputRating()}
+          {this.inputSelect()}
         </form>
       </div>
     );
