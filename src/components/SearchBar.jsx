8c333189e60c797d2034d84d7daf0bb5d07ba0c7
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-01-20 21:18:12 -0300

    requisito 4 - renderiza input do tipo checkbox

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,24 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
-  render() {
-    const { searchText, onSearchTextChange,
-      /* , bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange */
-    } = this.props;
+  inputText() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
-      <section>
-        <form data-testid="search-bar-form">
-          <label data-testid="text-input-label" htmlFor="text-input">
-            Inclui o texto:
-            <input
-              type="text"
-              value={ searchText }
-              onChange={ onSearchTextChange }
-              data-testid="text-input"
-            />
-          </label>
-        </form>
-      </section>
+      <label data-testid="text-input-label" htmlFor="text-input">
+        Inclui o texto:
+        <input
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+        />
+      </label>
+    );
+  }
+
+  inputCheckbox() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    return (
+      <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+        Mostrar somente favoritos
+        <input
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+        />
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        { this.inputText() }
+        { this.inputCheckbox() }
+      </form>
     );
   }
 }
@@ -27,8 +45,8 @@ class SearchBar extends React.Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string.isRequired,
   onSearchTextChange: PropTypes.func.isRequired,
-  // bookmarkedOnly: PropTypes.bool.isRequired,
-  // onBookmarkedChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
   // selectedGenre: PropTypes.string.isRequired,
   // onSelectedGenreChange: PropTypes.func.isRequired,
 };
