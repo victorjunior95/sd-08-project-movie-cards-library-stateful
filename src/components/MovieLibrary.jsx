f3699d487f16ea1dd01d478ceef5fe6f820c3f08
amandaos <amanda.775.souza@gmail.com>
2021-01-21 16:13:10 -0300

    resoluçao dos requisitos 15 ao 20

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends Component {
   constructor() {
@@ -11,6 +12,27 @@ class MovieLibrary extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  filterMovie() {
+    const { movies } = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    let filter = [...movies];
+    if (searchText) {
+      filter = filter.filter(({ title, subtitle, storyline }) => (
+        title.includes(searchText)
+        || subtitle.includes(searchText)
+        || storyline.includes(searchText)
+      ));
+    }
+    if (bookmarkedOnly) {
+      filter = filter.filter(({ bookmarked }) => bookmarked);
+    }
+
+    if (selectedGenre) {
+      filter = filter.filter(({ genre }) => genre.includes(selectedGenre));
+    }
+    return filter;
   }
 
   render() {
@@ -25,9 +47,16 @@ class MovieLibrary extends Component {
           onBookmarkedChange={ this.handleChange }
           onSelectedGenreChange={ this.handleChange }
         />
+        <MovieList movies={ this.filterFilms() } />
       </section>
     );
   }
 }
 
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
 export default MovieLibrary;
+
+// estudos para resoluções dos requisitos do MovieLibrary: https://github.com/tryber/sd-08-project-movie-cards-library-stateful/compare/rosieledavid-movie-cards-library-stateful#diff-f93a41729e61cd4c21f08e8fbbf630da87e14d18a479267fe157d29cafc00abc
--- src/components/SelectGenre.js
+++ src/components/SelectGenre.js
@@ -2,39 +2,30 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class SelectGenre extends Component {
-  renderOption(selected, onSelected) {
-    return (
-      <select
-        data-testid="select-input"
-        value={ selected }
-        onChange={ onSelected }
-      >
-        <option data-testid="select-option" value="">Todos</option>
-        <option data-testid="select-option" value="action">Ação</option>
-        <option data-testid="select-option" value="comedy">Comédia</option>
-        <option data-testid="select-option" value="thriller">Suspense</option>
-      </select>
-    );
-  }
-
   render() {
-    const {
-      selected,
-      onSelected,
-    } = this.props;
-
+    const { value, onChange } = this.props;
     return (
       <label data-testid="select-input-label" htmlFor="text-input-label">
         Filtrar por gênero
-        { this.renderOption(selected, onSelected) }
+        <select
+          data-testid="select-input"
+          value={ value }
+          onChange={ onChange }
+          name="selectedGenre"
+        >
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
       </label>
     );
   }
 }
 
 SelectGenre.propTypes = {
-  selected: PropTypes.string.isRequired,
-  onSelected: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default SelectGenre;
