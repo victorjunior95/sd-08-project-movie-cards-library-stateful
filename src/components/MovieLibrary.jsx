5bcc1f75ec2d7779e153c142263dfa380934a4e0
Ronald Jr <rpintojunior@gmail.com>
2021-01-21 09:04:28 -0300

    Adiciona MovieLibrary 1

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -4,17 +4,6 @@ import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
-  constructor() {
-    super();
-    // this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: '',
-    };
-  }
-
   render() {
     const { movies } = this.props;
     return (
