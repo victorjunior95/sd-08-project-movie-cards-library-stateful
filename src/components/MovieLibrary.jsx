0019bf99ddd00eb3d89d080ad42ecd2a4025b14a
Joao <joaocarlitoneto@gmail.com>
2021-01-21 20:08:01 -0300

    Req ate 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      // searchText: '',
+      // bookmarkedOnly: false,
+      // selectedGenre: '',
+      movies,
+    };
+  }
+
   render() {
+    const { movies } = this.state;
+    console.log(movies);
     return (
       <div>
+        <h2> My awesome movie library </h2>
         <SearchBar />
         <AddMovie />
+        <MovieList movies={ movies } />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.objects).isRequired,
+};
 
 export default MovieLibrary;
