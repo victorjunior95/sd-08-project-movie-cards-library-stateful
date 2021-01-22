9a8d941e242455ad85ed7e0a53608318c09a17cf
Hugo Andrade Braga <hugobrr@gmail.com>
2021-01-22 07:58:06 -0300

    para a vivi ver com o

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,17 @@
 // implement AddMovie component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <div data-testid="search-bar" className="search-bar">
+        <h3>AddMovie</h3>
+      </div>
+    );
+  }
+}
+
+AddMovie.protoType = { };
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,38 @@
 // implement SearchBar component here
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-from" className="search-bar">
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Incluir texto
+          <input type="text" value="searchText" onChange="onSearchTextChange" data-testid="text-input" />
+        </label>
+        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input checked="bookmarkedOnly" onChange="onBookmarkedChange" data-testid="checkbox-input" /> </label>
+        <label htmlFor="select-input" data-testid="select-input-label">
+          Filtrar por gênero
+          <select value="selectedGenre" data-testid="select-input" onChange="onSelectedGenreChange">
+            <option value=""  data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+SearchBar.protoType = {
+  searchText: PropTypes.string,
+  bookmarkedOnly: PropTypes.boleano,
+
+};
+
+export default SearchBar;
