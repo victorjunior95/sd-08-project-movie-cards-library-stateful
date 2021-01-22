861b6094d33b59d1c7db51c2b6bbc46059e209e8
Geziel <geziel.fernandez@pucp.pe>
2021-01-22 11:27:24 -0300

    testing MovieLibrary file

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,35 +1,19 @@
 // implement MovieLibrary component here
-// import React from 'react';
-// import SearchBar from './SearchBar';
+import React from 'react';
+import SearchBar from './SearchBar';
 // import MovieList from './MovieList';
-// import AddMovie from './AddMovie';
+import AddMovie from './AddMovie';
 
-// class MovieLibrary extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       searchText: '',
-//       bookmarkedOnly: false,
-//       selectedGenre: '',
-//       movies: props.movies,
-//     };
-//   }
+class MovieLibrary extends React.Component {
+  render() {
+    // const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
+}
 
-//   render() {
-//     const { searchText, bookmarkedOnly, selectedGenre } = this.state;
-//     return (
-//       <div>
-//         <SearchBar
-//           searchText={ searchText }
-//           onSearchTextChange={ this.onSearchTextChange }
-//           bookmarkedOnly={ bookmarkedOnly }
-//           onBookmarkedChange={ this.onBookmarkedChange }
-//           selectedGenre={ selectedGenre }
-//           onSelectedGenreChange={ this.onSelectedGenreChange }
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-// export default MovieLibrary;
+export default MovieLibrary;
