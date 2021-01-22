5123a83fc4529dbed4ab6123805129121e262e3e
Felip França <felipefrancafj@gmail.com>
2021-01-22 09:39:24 -0300

    REQ-1 - Create a SeachBar Component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SeachBar extends Component {
+  render() {
+    return (
+      <form id="search-bar-form">
+        <imput />
+      </form>
+    );
+  }
+}
+
+export default SeachBar;
