ad7ab11054c45baef0dc141c919ff4860c374a76
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-01-21 22:20:33 -0300

    requisito 15 e 16 - adiciona componente <MovieLibrary /> e configura status inicial

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,36 @@
-// implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+    /*       searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '', */
+      movies,
+    };
+  }
+
+  render() {
+    const { /* searchText, bookmarkedOnly, selectedGenre, */ movies } = this.state;
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+        <MovieList movies={ movies } />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieLibrary;
