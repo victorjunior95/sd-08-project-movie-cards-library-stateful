755f2e9e47cdb385fc7061100ad3725cd264a912
Hugo Andrade Braga <hugobrr@gmail.com>
2021-01-22 08:29:56 -0300

    com as propTypes do SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -30,9 +30,12 @@ class SearchBar extends React.Component {
 }
 
 SearchBar.protoType = {
-  searchText: PropTypes.string,
-  bookmarkedOnly: PropTypes.boleano,
-
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
