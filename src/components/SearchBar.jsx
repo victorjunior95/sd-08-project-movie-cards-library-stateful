6b21de9fa5104ec292548895294623115a9f123b
Felip França <felipefrancafj@gmail.com>
2021-01-22 12:11:46 -0300

    REQ-3 - Create a input type text to form on SearchBar Component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class SeachBar extends Component {
+class SearchBar extends Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <imput />
+        { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
+        <label htmlFor="text-input" data-testid="text-input-label">
+          Inclui o texto
+        </label>
+        <input
+          id="text-input"
+          type="text"
+          value={ searchText }
+          data-testid="text-input"
+          onChange={ onSearchTextChange }
+        />
       </form>
     );
   }
 }
 
-export default SeachBar;
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func.isRequired,
+};
+
+SearchBar.defaultProps = {
+  searchText: '',
+};
+
+export default SearchBar;
