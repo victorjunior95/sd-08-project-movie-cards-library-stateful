16572d5a7a7b8c3cd176c3d6750f3d88c6a56504
Patrícia <silva.patricia.v@gmail.com>
2021-01-23 10:01:23 -0300

    Testando Cypress

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,47 +1,48 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-    // const { movies } = this.props;
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      // movies,
-    };
+  // // constructor(props) {
+  // //   super(props);
+  // //   // const { movies } = this.props;
+  // //   this.state = {
+  // //     searchText: '',
+  // //     bookmarkedOnly: false,
+  // //     selectedGenre: '',
+  // //     // movies,
+  // //   };
 
-    this.onSearchTextChange = this.onSearchTextChange.bind(this);
-    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
-    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
-    this.newMovieAdd = this.newMovieAdd.bind(this);
-  }
+  //   this.onSearchTextChange = this.onSearchTextChange.bind(this);
+  //   this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+  //   this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  //   this.newMovieAdd = this.newMovieAdd.bind(this);
+  // }
 
-  onSearchTextChange(event) {
-    this.setState({ searchText: event.target.value,
-    });
-  }
+  // onSearchTextChange(event) {
+  //   this.setState({ searchText: event.target.value,
+  //   });
+  // }
 
-  onBookmarkedChange() {
-    this.setState({ bookmarkedOnly: !bookmarkedOnly,
-    });
-  }
+  // onBookmarkedChange() {
+  //   this.setState({ bookmarkedOnly: !bookmarkedOnly,
+  //   });
+  // }
 
-  onSelectedGenreChange(event) {
-    this.setState({ selectedGenre: event.target.value,
-    });
-  }
+  // onSelectedGenreChange(event) {
+  //   this.setState({ selectedGenre: event.target.value,
+  //   });
+  // }
 
   newMovieAdd() {
 
   }
 
   render() {
-    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    // const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const { movies } = this.props;
     return (
       <div>
         <SearchBar
@@ -52,15 +53,15 @@ class MovieLibrary extends Component {
           onSelectedGenreChange={ this.onSelectedGenreChange }
           selectedGenre={ selectedGenre }
         />
-        <MovieList />
+        <MovieList movies={ movies } />
         <AddMovie onClick={ this.newMovieAdd } />
       </div>
     );
   }
 }
 
-// MovieLibrary.propTypes = {
-//   movies: PropTypes.arrayOf.isRequired,
-// };
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.objects).isRequired,
+};
 
 export default MovieLibrary;
