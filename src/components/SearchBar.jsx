e472133a7a25ea7c228629f7b3cce21a25804a5e
Felip França <felipefrancafj@gmail.com>
2021-01-23 08:44:17 -0300

    REQ-4 Create input type ckeckbox at SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,20 +2,46 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
+  InputText(searchText, onSearchTextChange) {
+    return (
+      <label htmlFor="text_input" data-testid="text-input-label">
+        Inclui o texto
+        <input
+          id="text_input"
+          type="text"
+          value={ searchText }
+          data-testid="text-input"
+          onChange={ onSearchTextChange }
+        />
+      </label>
+    );
+  }
+
+  InputCheckBox(bookmarkedOnly, onBookmarkedChange) {
+    return (
+      <label htmlFor="checked_input" data-testid="checkbox-input-label">
+        Mostrar somente favoritos
+        <input
+          id="checked_input"
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          data-testid="checkbox-input"
+          onChange={ onBookmarkedChange }
+        />
+      </label>
+    );
+  }
+
   render() {
-    const { searchText, onSearchTextChange } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label htmlFor="text_input" data-testid="text-input-label">
-          Inclui o texto
-          <input
-            id="text_input"
-            type="text"
-            value={ searchText }
-            data-testid="text-input"
-            onChange={ onSearchTextChange }
-          />
-        </label>
+        { this.InputText(searchText, onSearchTextChange) }
+        { this.InputCheckBox(bookmarkedOnly, onBookmarkedChange) }
       </form>
     );
   }
@@ -24,6 +50,8 @@ class SearchBar extends Component {
 SearchBar.propTypes = {
   searchText: PropTypes.string,
   onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.string.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
 };
 
 SearchBar.defaultProps = {
