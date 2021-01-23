992f09b2ab7dc0c2a031c6b6ad771ed4953cebd8
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-23 11:26:59 -0300

    partial implementation of components from Req16 to Req19

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import movies from './data';
 
 import Header from './components/Header';
+import MovieLibrary from './components/MovieLibrary';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <MovieLibrary movies={ movies }/>
       </div>
     );
   }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
-    super(props)
-
+    super(props);
+    const { movies } = this.props;
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: this.props.movies
-    }
+      movies,
+    };
   }
 
   render() {
-    return(
-      null
-    )
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <>
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </>
+    );
   }
 }
 
-export default MovieLibrary
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieLibrary;
