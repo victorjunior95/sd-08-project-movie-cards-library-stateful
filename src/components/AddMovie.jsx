a1f73463d3afba09719c56ced9c28381649e93f8
Giovanna Betti <giovannabetti@gmail.com>
2021-01-24 01:41:57 -0300

    requisites 6 to 13 completed, 14 failing two tests about button - not implemented yet

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -13,7 +13,7 @@ class AddMovie extends React.Component {
     };
   }
 
-  titleInput() {
+  titleInput(title) {
     return (
       <label htmlFor="title-input" data-testid="title-input-label">
         Título
@@ -27,31 +27,90 @@ class AddMovie extends React.Component {
     );
   }
 
+  subtitleInput(subtitle) {
+    return (
+      <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+        Subtítulo
+        <input
+          data-testid="subtitle-input"
+          type="text"
+          value={ subtitle }
+          onChange={ (e) => this.setState({ subtitle: e.target.value }) }
+        />
+      </label>
+    );
+  }
+
+  imagePathInput(imagePath) {
+    return (
+      <label htmlFor="image-input" data-testid="image-input-label">
+        Imagem
+        <input
+          data-testid="image-input"
+          type="text"
+          value={ imagePath }
+          onChange={ (e) => this.setState({ imagePath: e.target.value }) }
+        />
+      </label>
+    );
+  }
+
+  storylineInput(storyline) {
+    return (
+      <label htmlFor="image-input" data-testid="storyline-input-label">
+        Sinopse
+        <input
+          data-testid="storyline-input"
+          type="textarea"
+          value={ storyline }
+          onChange={ (e) => this.setState({ storyline: e.target.value }) }
+        />
+      </label>
+    );
+  }
+
+  ratingInput(rating) {
+    return (
+      <label htmlFor="rating-input" data-testid="rating-input-label">
+        Avaliação
+        <input
+          data-testid="rating-input"
+          type="number"
+          value={ rating }
+          onChange={ (e) => this.setState({ rating: e.target.value }) }
+        />
+      </label>
+    );
+  }
+
+  genreInput(genre) {
+    return (
+      <label htmlFor="genre-input" data-testid="genre-input-label">
+        Gênero
+        <select
+          data-testid="genre-input"
+          value={ genre }
+          onChange={ (e) => this.setState({ genre: e.target.value }) }
+        >
+          <option data-testid="genre-option" value="action" defaultChecked>Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
-    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        {this.titleInput()}
-        <label>
-          <input type="text" />
-        </label>
-
-        <label>
-          <input type="text" />
-        </label>
-
-        <label>
-          <input type="textarea" />
-        </label>
-
-        <label>
-          <input type="number" />
-        </label>
-
-        <label>
-          <select>Gender</select>
-        </label>
-        <button type="submit">Send</button>
+        {this.titleInput(title)}
+        {this.subtitleInput(subtitle)}
+        {this.imagePathInput(imagePath)}
+        {this.storylineInput(storyline)}
+        {this.ratingInput(rating)}
+        {this.genreInput(genre)}
+        <button data-testid="send-button" type="submit">Adicionar filme</button>
       </form>
     );
   }
