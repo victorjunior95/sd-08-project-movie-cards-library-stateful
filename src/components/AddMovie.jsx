6fe08c66680cfe1e6cb0a4a50236a0c55375e5ed
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-01-24 18:23:29 -0300

    corrige issues

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,51 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            subtitle: '',
-            title: '',
-            imagePath: '',
-            storyLine: '',
-            rating: 0,
-            genre: 'action',
-        };
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
 
-        this.onChange = this.onChange.bind(this);
-        this.inputText = this.inputText.bind(this);
-        this.inputTextArea = this.inputTextArea.bind(this);
-        this.inputNumber = this.inputNumber.bind(this);
-        this.inputSelect = this.inputSelect.bind(this);
-        this.formSubmission = this.formSubmission .bind(this);
-    }
+    this.onChange = this.onChange.bind(this);
+    this.inputText = this.inputText.bind(this);
+    // this.inputTextArea = this.inputTextArea.bind(this);
+    // this.inputNumber = this.inputNumber.bind(this);
+    // this.inputSelect = this.inputSelect.bind(this);
+    // this.formSubmission = this.formSubmission.bind(this);
+  }
 
-    onChange({ target }) {
-        const { name, value } = target;
-        this.setState({
-            [name]: value,
-        });
-    }
+  onChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
 
-    inputText(title) {
-        return (
-            <label htmlFor="title-input" data-testid="title-input-label">
+  inputText(title) {
+    return (
+      <label htmlFor="title-input" data-testid="title-input-label">
                 Título
-                <input
-                  type="text"
-                  value={ title }
-                  id="title-input"
-                  data-testid="title-input"
-                  name="title"
-                  onChange={ this.onChange }
-                  />
-            </label>
-        )
-    }
+        <input
+          type="text"
+          value={ title }
+          id="title-input"
+          data-testid="title-input"
+          name="title"
+          onChange={ this.onChange }
+        />
+      </label>
+    );
+  }
 }
 
-AddMovie.propTypes = {
-    onClick: PropTypes.func.isRequired,
-}
+// AddMovie.propTypes = {
+//     onClick: PropTypes.func.isRequired,
+// };
 
 export default AddMovie;
