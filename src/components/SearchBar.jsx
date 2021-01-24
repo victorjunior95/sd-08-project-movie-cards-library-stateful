a39123e3e58d641774cb866eaa0744dcca95b32a
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-01-24 17:17:53 -0300

    acabou a criatividade pra nomear commits

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
-
   inputText(searchText, onSearchTextChange) {
     return (
       <div>
