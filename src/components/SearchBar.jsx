b9601fb695cafc4f34d908d72244454967c52244
Giovanna Betti <giovannabetti@gmail.com>
2021-01-23 23:41:31 -0300

    refactoring requisite 01 with functions outside render method

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  textInputLabel(searchText, onSearchTextChange) {
+  textInput(searchText, onSearchTextChange) {
     return (
       <label data-testid="text-input-label">
         Inclui o texto:
@@ -15,24 +15,45 @@ class SearchBar extends React.Component {
     );
   }
 
+  checkBoxInput(bookmarkedOnly, onBookmarkedChange) {
+    return (
+      <label data-testid="checkbox-input-label">
+        Mostrar somente favoritos
+        <input
+          data-testid="checkbox-input"
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+        />
+      </label>
+    );
+  }
+
+  selectInput(selectedGenre, onSelectedGenreChange) {
+    return (
+      <label data-testid="select-input-label">
+        Filtrar por gênero
+        <select
+          data-testid="select-input"
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+        >
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
-        {this.textInputLabel(searchText, onSearchTextChange)}
-        <label data-testid="checkbox-input-label">
-          Mostrar somente favoritos
-          <input data-testid="checkbox-input" type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } />
-        </label>
-        <label data-testid="select-input-label" >
-          Filtrar por gênero
-          <select data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange} >
-            <option data-testid="select-option" value="">Todos</option>
-            <option data-testid="select-option" value="action">Ação</option>
-            <option data-testid="select-option" value="comedy">Comédia</option>
-            <option data-testid="select-option" value="thriller">Suspense</option>
-          </select>
-        </label>
+        {this.textInput(searchText, onSearchTextChange)}
+        {this.checkBoxInput(bookmarkedOnly, onBookmarkedChange)}
+        {this.selectInput(selectedGenre, onSelectedGenreChange)}
       </form>
     );
   }
