13c0138f30e8290cd1064137ee5602659f23ef33
enicio <jessandro42@gmail.com>
2021-01-24 20:14:41 -0300

    Requisito 14

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,38 +1,99 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submit = this.submit.bind(this);
+
+    this.state = {
+      title: '',
+      subtitle: '',
+      sinopse: '',
+      rating: 0,
+      image: '',
+      gender: 'action',
+    };
+  }
+
+  handleChange({ target }) {
+    this.setState(
+      { [target.name]: target.value },
+    );
+  }
+
+  submit(event) {
+    event.preventDefault();
+    this.setState({
+      title: '',
+      subtitle: '',
+      sinopse: '',
+      rating: 0,
+      image: '',
+      gender: '',
+    });
+  }
+
   inputTitle() {
+    const { title } = this.state;
     return (
       <label data-testid="title-input-label" htmlFor="title">
         Título
-        <input data-testid="title-input" id="title" type="text" />
+        <input
+          onChange={ this.handleChange }
+          value={ title }
+          name="title"
+          data-testid="title-input"
+          id="title"
+          type="text"
+        />
       </label>
     );
   }
 
   inputSubtitle() {
+    const { subtitle } = this.state;
     return (
       <label data-testid="subtitle-input-label" htmlFor="subtitle">
         Subtítulo
-        <input data-testid="subtitle-input" id="subtitle" type="text" />
+        <input
+          onChange={ this.handleChange }
+          value={ subtitle }
+          name="subtitle"
+          data-testid="subtitle-input"
+          id="subtitle"
+          type="text"
+        />
       </label>
     );
   }
 
   inputImage() {
+    const { image } = this.state;
     return (
       <label data-testid="image-input-label" htmlFor="image">
         Imagem
-        <input data-testid="image-input" id="image" type="text" />
+        <input
+          onChange={ this.handleChange }
+          value={ image }
+          name="image"
+          data-testid="image-input"
+          id="image"
+          type="text"
+        />
       </label>
     );
   }
 
   inputSinopse() {
+    const { sinopse } = this.state;
     return (
       <label data-testid="storyline-input-label" htmlFor="sinopse">
         Sinopse
         <textarea
+          value={ sinopse }
+          onChange={ this.handleChange }
           data-testid="storyline-input"
           name="sinopse"
           id="sinopse"
@@ -44,19 +105,35 @@ class AddMovie extends React.Component {
   }
 
   inputRating() {
+    const { rating } = this.state;
     return (
       <label data-testid="rating-input-label" htmlFor="rating">
         Avaliação
-        <input data-testid="rating-input" id="rating" type="number" defaultValue="0" />
+        <input
+          onChange={ this.handleChange }
+          value={ rating }
+          name="rating"
+          data-testid="rating-input"
+          id="rating"
+          type="number"
+          defaultValue="0"
+        />
       </label>
     );
   }
 
   inputSelect() {
+    const { gender } = this.state;
     return (
       <label data-testid="genre-input-label" htmlFor="gender">
         Gênero
-        <select data-testid="genre-input" name="gender" id="gender">
+        <select
+          onChange={ this.handleChange }
+          value={ gender }
+          data-testid="genre-input"
+          name="gender"
+          id="gender"
+        >
           <option data-testid="genre-option" value="action">Ação</option>
           <option data-testid="genre-option" value="comedy">Comédia</option>
           <option data-testid="genre-option" value="thriller">Suspense</option>
@@ -76,6 +153,9 @@ class AddMovie extends React.Component {
           {this.inputSinopse()}
           {this.inputRating()}
           {this.inputSelect()}
+          <button data-testid="send-button" type="submit" onClick={ this.submit }>
+            Adicionar filme
+          </button>
         </form>
       </div>
     );
