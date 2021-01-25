761214e5eb44006c8baf8ea49270b3451b1ccdca
Hugo Andrade Braga <hugobrr@gmail.com>
2021-01-25 18:31:12 -0300

    requisito 16 sendo ingenuo

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,6 +3,16 @@ import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+    const movies = props.movies;
+  }
+
   render() {
     return (
       <div>
