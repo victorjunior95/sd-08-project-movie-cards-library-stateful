0831573cb6c9b959d18eb2775056d2966a625763
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-01-25 09:23:05 -0300

    C7 - 7th Req Fixed

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class AddMovie extends React.Component {
   render() {
     return (
-      <form data-testid="search-bar-form">
+      <form data-testid="add-movie-form">
         <p>teste do formulário do AddMovie</p>
       </form>
     );
