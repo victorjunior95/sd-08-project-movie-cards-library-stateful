55aa9fab940b8bdb83c61cc3a26e9908847b4d0e
Blucilha <blucilha.nascimento@gmail.com>
2021-01-25 13:30:29 -0300

    Exercicio MovieLibrary parte 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import movies from './data';
 
 import Header from './components/Header';
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MovieLibrary />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
