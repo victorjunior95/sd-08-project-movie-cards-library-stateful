ec133fc2f2653003ac97a8b709b93b58af5fca69
Blucilha <blucilha.nascimento@gmail.com>
2021-01-25 18:38:12 -0300

    Exercicio MovieLibrary parte 5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-
+import movies from './data';
 import MovieLibrary from './components/MovieLibrary';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieLibrary />
+      <MovieLibrary movies={ movies } />
     </div>
   );
 }
 
 export default App;
+
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
+    this.addNewMovie = this.addNewMovie.bind(this);
+  }
+
+  addNewMovie(newMovie) {
+    const { movies } = this.state;
+    this.setState({ movies: movies.concat(newMovie) });
+  }
+
   render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
-        <SearchBar />
-        <AddMovie />
+        <AddMovie onClick={ this.addNewMovie } />
+        <SearchBar
+          searchText={ searchText }
+          bookmarkedOnly={ bookmarkedOnly }
+          selectedGenre={ selectedGenre }
+        />
+        <MovieList movies={ movies } />
       </div>
     );
   }
 }
 
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf.isRequired,
+};
 export default MovieLibrary;
+
