8969d261056ff6302369efa08094addc21938732
Blucilha <blucilha.nascimento@gmail.com>
2021-01-25 13:15:37 -0300

    Exercicio MovieLibrary (apenas o 15)

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,16 @@
-// implement MovieLibrary component here
+import React from 'react';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
