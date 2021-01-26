07e7e287799b387cfd51d310162e1723abb020d5
Rafael <rafaeljavaro750@gmail.com>
2021-01-26 18:47:38 -0300

    Componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 class SearchBar extends React.Component {
     inputText(searchText, onSearchTextChange) {
         return (
-          <label htmlFor="" data-testid="text-input-label">
+          <label data-testid="text-input-label">
             Inclui o texto:  
             <input 
             type="text"
@@ -16,15 +16,54 @@ class SearchBar extends React.Component {
         );
     }
 
+    inputCheckBox(bookmarkedOnly, onBookmarkedChange) {
+        return (
+            <label data-testid="checkbox-input-label">
+                Mostrar somente favoritos
+                <input
+                type="checkbox"
+                checked={ bookmarkedOnly }
+                onChange={ onBookmarkedChange }
+                data-testid="checkbox-input"
+                />
+            </label>
+        );
+    }
+
+    inputselectGenre(selectedGenre, onSelectedGenreChange ) {
+        return (
+            <label data-testid="select-input-label">
+                Filtrar por gênero
+                <select
+                data-testid="select-input"
+                value={ selectedGenre }
+                onChange={ onSelectedGenreChange }
+                >
+                    <option data-testid="select-option" value="">Todos</option>
+                    <option data-testid="select-option" value="action">Ação</option>
+                    <option data-testid="select-option" value="comedy">Comédia</option>
+                    <option data-testid="select-option" value="thriller">Suspense</option>
+                </select>
+            </label>
+        );
+    }
+
+
     render() {
         const {
             searchText,
             onSearchTextChange,
+            bookmarkedOnly,
+            onBookmarkedChange,
+            selectedGenre,
+            onSelectedGenreChange,
         } = this.props;
 
         return (
             <form data-testid="seach-bar-form" >
                 {this.inputText(searchText, onSearchTextChange)}
+                {this.inputCheckBox(bookmarkedOnly, onBookmarkedChange)}
+                {this.inputselectGenre(selectedGenre, onSelectedGenreChange)}
             </form>
         );
     }
