dab528ebd747ba6f4f0a938531ac0d10942e2a0f
Rafael <rafaeljavaro750@gmail.com>
2021-01-26 17:26:21 -0300

    Renderize o componente , recebendo as devidas props

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,15 +3,7 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
     render() {
-        const {
-            searchText,
-            onSearchTextChange,
-            bookmarkedOnly,
-            onBookmarkedChange,
-            selectedGenre,
-            onSelectedGenreChange,
-        } = this.props;
-
+        
         return (
             <form data-testid="seach-bar-form" ></form>
         );
