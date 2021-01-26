e68944d5ccc1ec531c91fb24a313d8ce2471fb67
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-01-26 03:44:47 -0300

    Requisito 1

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -4,7 +4,7 @@ import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
-export default class MovieLibrary extends Component {
+class MovieLibrary extends Component {
   constructor(props) {
     super(props);
   }
@@ -14,9 +14,11 @@ export default class MovieLibrary extends Component {
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <MovieList />
         <AddMovie />
       </div>
     );
   }
 }
+
+export default MovieLibrary;
