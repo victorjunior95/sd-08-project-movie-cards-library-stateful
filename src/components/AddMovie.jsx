87b9ae0d5eb2cecd283c13f8b6706c9c9cb45a4b
Victor Moraes <moraesvictorgk@gmail.com>
2021-01-26 23:17:31 -0300

    Adiciona requisitos 1 ao 12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,167 @@
 // implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  }
+
+  handleClick(add) {
+    add(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  inputTextTitle() {
+    const { title } = this.state;
+    return (
+      <label htmlFor="title" data-testid="title-input-label">
+        Título
+        <input
+          id="title"
+          type="text"
+          value={ title }
+          onChange={ this.handleChange }
+          data-testid="title-input"
+        />
+      </label>
+
+    );
+  }
+
+  inputTextSubtitle() {
+    const { subtitle } = this.state;
+    return (
+      <label htmlFor="subtitle" data-testid="subtitle-input-label">
+        Subtítulo
+        <input
+          id="subtitle"
+          type="text"
+          value={ subtitle }
+          onChange={ this.handleChange }
+          data-testid="subtitle-input"
+        />
+      </label>
+
+    );
+  }
+
+  inputTextImagePath() {
+    const { imagePath } = this.state;
+    return (
+      <label htmlFor="imagePath" data-testid="image-input-label">
+        Imagem
+        <input
+          id="imagePath"
+          type="text"
+          value={ imagePath }
+          onChange={ this.handleChange }
+          data-testid="image-input"
+        />
+      </label>
+
+    );
+  }
+
+  textAreaSinopse() {
+    const { storyline } = this.state;
+    return (
+      <label htmlFor="storyline" data-testid="storyline-input-label">
+        Sinopse
+        <textarea
+          id="storyline"
+          value={ storyline }
+          onChange={ this.handleChange }
+          data-testid="storyline-input"
+        />
+      </label>
+
+    );
+  }
+
+  inputNumberRating() {
+    const { rating } = this.state;
+    return (
+      <label htmlFor="rating" data-testid="rating-input-label">
+        Avaliação
+        <input
+          id="rating"
+          type="number"
+          value={ rating }
+          onChange={ this.handleChange }
+          data-testid="rating-input"
+        />
+      </label>
+
+    );
+  }
+
+  selectGenre() {
+    const { genre } = this.state;
+    return (
+      <label htmlFor="genre" ddata-testid="genre-input-label">
+        Gênero
+        <select
+          id="genre"
+          value={ genre }
+          onChange={ this.handleChange }
+          data-testid="genre-input"
+        >
+          <option value="action" data-testid="genre-option">Ação</option>
+          <option value="comedy" data-testid="genre-option">Comédia</option>
+          <option value="thriller" data-testid="genre-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form data-testid="add-movie-form">
+        { this.inputTextTitle() }
+        { this.inputTextSubtitle() }
+        { this.inputTextImagePath() }
+        { this.textAreaSinopse() }
+        { this.inputNumberRating() }
+        { this.selectGenre() }
+        <button
+          type="button"
+          onClick={ () => { this.handleClick(onClick); } }
+        >
+          Adicionar filme
+        </button>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,78 @@
 // implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  TextInput() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <label htmlFor="search" data-testid="text-input-label">
+        Inclui o texto:
+        <input
+          id="search"
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+        />
+      </label>
+    );
+  }
+
+  CheckboxInput() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    return (
+      <label htmlFor="favorite" data-testid="checkbox-input-label">
+        Mostrar somente favoritos
+        <input
+          id="favorite"
+          type="checkbox"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+        />
+      </label>
+    );
+  }
+
+  selectInput() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label htmlFor="filter" data-testid="select-input-label">
+        Filtrar por gênero
+        <select
+          id="filter"
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+          data-testid="select-input"
+        >
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        { this.TextInput() }
+        { this.CheckboxInput() }
+        { this.selectInput() }
+      </form>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
