e11adbbac1566539cb334655b25ae646709249bf
Rafael <rafaeljavaro750@gmail.com>
2021-01-28 13:14:18 -0300

    Reinicia Projeto

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
@@ -8,70 +7,18 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
-
-    const { movies } = this.props;
-
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies,
-    };
-
-    this.handleSearchEvent = this.handleSearchEvent.bind(this);
-    this.handleAddMovie = this.handleAddMovie.bind(this);
-    this.handleFilter = this.handleFilter.bind(this);
-  }
-
-  handleSearchEvent({ target }) {
-    this.setState({
-      [target.name]: target.type === 'checkbox' ? target.checked : target.value,
-    });
-  }
-
-  handleAddMovie(objectMovie) {
-    this.setState((currentState) => ({
-      movies: [...currentState.movies, objectMovie],
-    }));
-  }
-
-  handleFilter(movies) {
-    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
-    let moviesToShow = movies
-      .filter((movie) => movie.genre.includes(selectedGenre))
-      .filter((movie) => movie.title.includes(searchText)
-        || movie.subtitle.includes(searchText)
-        || movie.storyline.includes(searchText));
-    moviesToShow = !bookmarkedOnly
-      ? moviesToShow
-      : moviesToShow.filter((movie) => movie.bookmarked);
-
-    return moviesToShow;
   }
 
   render() {
-    const { searchText, bookmarkedOnly, movies, selectedGenre } = this.state;
-    const moviesToShow = this.handleFilter(movies);
     return (
       <div>
-        <h2> My  movie library </h2>
-        <SearchBar
-          searchText={ searchText }
-          bookmarkedOnly={ bookmarkedOnly }
-          selectedGenre={ selectedGenre }
-          onSearchTextChange={ this.handleSearchEvent }
-          onBookmarkedChange={ this.handleSearchEvent }
-          onSelectedGenreChange={ this.handleSearchEvent }
-        />
-        <MovieList movies={ moviesToShow } />
-        <AddMovie onClick={ this.handleAddMovie } />
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
       </div>
     );
   }
 }
-
-MovieLibrary.propTypes = {
-  movies: PropTypes.arrayOf().isRequired,
-};
 
 export default MovieLibrary;
