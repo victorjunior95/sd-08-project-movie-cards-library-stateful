93b273d7416c9cccb56cc1f29626de8183916e9d
OPedroAzevedo <pvas1996@gmail.com>
2021-02-02 19:53:17 -0300

    Primeiro Commit já com alguns requisitos pq eu esqueci de fazer commits até agr :v

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,71 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    };
+
+    this.movieNameInput = this.movieNameInput.bind(this);
+    this.subtitleInput = this.subtitleInput.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  //   handleChange(event) {
+  //       this.setState({title: event.target.title});
+  //   }
+
+  movieNameInput(callback) {
+    const { title } = this.state;
+    return (
+      <label data-testid="title-input-label" htmlFor="inputMovieName">
+        Título:
+        <input
+          type="text"
+          name="title"
+          value={ title }
+          data-testid="title-input"
+          onChange={ callback }
+          id="title-input"
+        />
+      </label>
+    );
+  }
+
+  subtitleInput(callback) {
+    const { subtitle } = this.state;
+    return (
+      <label data-testid="subtitle-input-label" htmlFor="inputSubtitle">
+        Subtitle:
+        <input
+          type="text"
+          name="subtitle"
+          value={ subtitle }
+          data-testid="subtitle-input"
+          onChange={ callback }
+          id="subtitle-input"
+        />
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <label htmlFor="inputMovie">
+        <form data-testid="add-movie-form">
+          { this.movieNameInput(this) }
+          { this.subtitleInput(this) }
+          {/* { this.movieNameInput(this.handleChange) } */}
+        </form>
+      </label>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,90 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+
+    this.inputText = this.inputText.bind(this);
+    this.inputCheckbox = this.inputCheckbox.bind(this);
+    this.genreSelector = this.genreSelector.bind(this);
+  }
+
+  inputText() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <label data-testid="text-input-label" htmlFor="text-input">
+        Inclui o texto:
+        <input
+          type="text"
+          name="searchText"
+          id="searchText"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+          data-testid="text-input"
+        />
+      </label>
+    );
+  }
+
+  inputCheckbox() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    return (
+      <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+        Mostrar somente favoritos:
+        <input
+          type="checkbox"
+          name="bookmarkedOnly"
+          id="checkbox-input"
+          checked={ bookmarkedOnly }
+          onChange={ onBookmarkedChange }
+          data-testid="checkbox-input"
+        />
+      </label>
+    );
+  }
+
+  genreSelector() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label data-testid="select-input-label" htmlFor="select-genre">
+        Filtrar por gênero:
+        <select
+          name="selectedGenre"
+          id="select-genre"
+          data-testid="select-input"
+          value={ selectedGenre }
+          onChange={ onSelectedGenreChange }
+        >
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <section className="search-form">
+        <form data-testid="search-bar-form">
+          { this.inputText() }
+          { this.inputCheckbox() }
+          { this.genreSelector() }
+        </form>
+      </section>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
