e70331c361a4ba6ca06b8f0cf71f69813e8d6843
OPedroAzevedo <pvas1996@gmail.com>
2021-02-03 01:42:00 -0300

    Correção requisitos 9, requisitos 10, 11, 12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,11 +6,16 @@ class AddMovie extends React.Component {
     this.state = {
       title: '',
       subtitle: '',
+      imagePath: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
 
     this.movieNameInput = this.movieNameInput.bind(this);
     this.subtitleInput = this.subtitleInput.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.imageInput = this.imageInput.bind(this);
+    this.textInput = this.textInput.bind(this);
+    this.ratingInput = this.ratingInput.bind(this);
   }
 
   handleChange(event) {
@@ -22,15 +27,14 @@ class AddMovie extends React.Component {
   movieNameInput(callback) {
     const { title } = this.state;
     return (
-      <label data-testid="title-input-label" htmlFor="inputMovieName">
+      <label data-testid="title-input-label" htmlFor="inputTitle">
         Título:
         <input
           type="text"
           name="title"
           value={ title }
-          data-testid="title-input"
           onChange={ callback }
-          id="title-input"
+          data-testid="title-input"
         />
       </label>
     );
@@ -40,22 +44,65 @@ class AddMovie extends React.Component {
     const { subtitle } = this.state;
     return (
       <label data-testid="subtitle-input-label" htmlFor="inputSubtitle">
-        Subtitle:
+        Subtítulo:
         <input
           type="text"
           name="subtitle"
           value={ subtitle }
-          data-testid="subtitle-input"
           onChange={ callback }
-          id="subtitle-input"
+          data-testid="subtitle-input"
         />
       </label>
     );
   }
 
-  //   imageInput(callback) {
+  imageInput(callback) {
+    const { imagePath } = this.state;
+    return (
+      <label data-testid="image-input-label" htmlFor="inputImage">
+        Imagem:
+        <input
+          type="text"
+          name="imagePath"
+          value={ imagePath }
+          data-testid="image-input"
+          onChange={ callback }
+        />
+      </label>
+    );
+  }
 
-  //   }
+  textInput(callback) {
+    const { storyline } = this.state;
+    return (
+      <label data-testid="storyline-input-label" htmlFor="inputText">
+        Sinopse:
+        <input
+          type="text"
+          name="storyline"
+          value={ storyline }
+          data-testid="storyline-input"
+          onChange={ callback }
+        />
+      </label>
+    );
+  }
+
+  ratingInput(callback) {
+    const { rating } = this.state;
+    return (
+      <label data-testid="rating-input-label" htmlFor="inputRating">
+        Avaliação:
+        <input
+          type="number"
+          name="rating"
+          value={ rating }
+          data-testid="rating-input"
+          onChange={ callback }
+        />
+      </label>
+    );
+  }
 
   render() {
     return (
@@ -63,6 +110,9 @@ class AddMovie extends React.Component {
         <form data-testid="add-movie-form">
           { this.movieNameInput(this.handleChange) }
           { this.subtitleInput(this.handleChange) }
+          { this.imageInput(this.handleChange)}
+          { this.textInput(this.handleChange)}
+          { this.ratingInput(this.handleChange)}
         </form>
       </label>
     );
