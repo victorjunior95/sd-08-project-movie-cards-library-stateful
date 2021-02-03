923202b882bef7db3d7d523a668ede89298ba948
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-02-03 20:15:49 -0300

    Movie Library fixes

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,39 @@
 // implement MovieLibrary component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+    this.changeState = this.changeState.bind(this);
+  }
+
+  changeState(name, type) {
+    return ((event) => {
+      this.setState({ [name]: type(event.target.value) });
+    });
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    return (
+      <SearchBar
+        searchText={ searchText }
+        onSearchTextChange={ this.changeState('searchText', String) }
+        bookmarkedOnly={ bookmarkedOnly }
+        onBookmarkedChange={ this.changeState('bookmarkedOnly', Boolean) }
+        selectedGenre={ selectedGenre }
+        onSelectedGenreChange={ this.changeState('selectedGenre', String) }
+      />
+    );
+  }
+}
+
+export default MovieLibrary;
