dc5dcc5f2f3eec7d28a4d3c35cf7feb1a043263f
Uibirá <usbarreto@gmail.com>
2021-02-03 19:20:51 -0300

    Req.3 tentativa

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <div>
+        <form data-testid="text-input-label" method="post" />
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
