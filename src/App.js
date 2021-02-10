e3e0b450289776e685e30d7a9c118b34ea0ecc0e
Thais Quintela <thais.s.quintela@gmail.com>
2021-02-10 20:37:42 -0300

    Requisitos 1, 2, 3, 4 e 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    }
+  }
+
+  onSearchTextChange({ target }) {
+    const { value } = target;
+
+    this.setState({
+      searchText: value,
+    })
+  }
+
+  onBookmarkedChange({ target }) {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      bookmarkedOnly: value,
+    })
+  }
+
+  onSelectedGenreChange({ target }) {
+    const { value } = target;
+
+    this.setState({
+      selectedGenre: value,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar value={ this.state.name } onChange={ this.onSearchTextChange, this.onBookmarkedChange, this.onSelectedGenreChange } />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,30 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">
+          Inclui o texto
+          <input type="text" value={ searchText } onChange={ onSearchTextChange } data-testid="text-input"/>
+        </label>
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input type="checkbox" checked={ bookmarkedOnly } onChange={ onBookmarkedChange } data-testid="checkbox-input"/>
+        </label>
+        <label data-testid="select-input-label">Filtrar por gênero
+          <select data-testid="select-input" value={ selectedGenre } onChange={ onSelectedGenreChange }>
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
