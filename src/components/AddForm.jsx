e0a00d730ba41d94a236d5fd53be68a9999267c5
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-02-11 20:04:36 -0300

    estragado

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import Title from './Title';
-import SubTitle from './SubTitle';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ImagePath from './ImagePath';
-import TextArea from './TextArea';
-import RatingNew from './RatingNew';
-import SelectGenreNew from './SelectGenreNew';
+import Rating from './Rating';
+import InputSelect from './InputSelect';
+import InputTextArea from './InputTextArea';
+import TextInputSubtitle from './TextInputSubtitle';
+import TextInputTitle from './TextInputTitle';
 
-class AddForm extends React.Component {
+export default class RenderAll extends Component {
   render() {
+    const { func, funcGenre, title, sub, image, storyline, rating, genre } = this.props;
     return (
       <div>
-        <Title />
-        <SubTitle />
-        <ImagePath />
-        <TextArea />
-        <RatingNew />
-        <SelectGenreNew />
+        <TextInputTitle func={ func } title={ title } />
+        <TextInputSubtitle func={ func } subtitle={ sub } />
+        <InputImage func={ func } imagePath={ image } />
+        <InputTextArea func={ func } storyline={ storyline } />
+        <InputRating func={ func } rating={ rating } />
+        <InputSelect func={ funcGenre } genre={ genre } />
       </div>
     );
   }
 }
 
-export default AddForm;
+RenderAll.propTypes = {
+  func: PropTypes.func.isRequired,
+  funcGenre: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  sub: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  genre: PropTypes.string.isRequired,
+};
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
 import AddForm from './AddForm';
 
-class AddMovie extends React.Component {
-  render() {
+export default class AddMovie extends Component {
+  constructor() {
+    super();
     this.state = {
       subtitle: '',
       title: '',
@@ -11,12 +14,56 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleGenre = this.handleGenre.bind(this);
+    this.clearState = this.clearState.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleGenre(event) {
+    this.setState({
+      genre: event.target.value,
+    });
+  }
+
+  clearState() {
+    const { onClick } = this.props;
+    onClick();
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  render() {
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <AddForm />
+        <AddForm
+          func={ this.handleChange }
+          funcGenre={ this.handleGenre }
+          title={ title }
+          sub={ subtitle }
+          image={ imagePath }
+          storyline={ storyline }
+          rating={ rating }
+          genre={ genre }
+        />
+        <Button onClick={ this.clearState } />
       </form>
     );
   }
 }
 
-export default AddMovie;
+AddMovie.propTypes = {
+  onClick: PropTypes.func,
+}.isRequired;
