2a5c5453e124156ffec90b30fb3ce69f08b2ed98
v-icto-r <victor130496@gmail.com>
2021-02-13 16:06:18 -0300

    Subindo Add Movie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,166 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const initialState = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyLine: '',
+  rating: 0,
+  genre: 'action',
+}
+
+class AddMovie extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      ...initialState,
+    }
+    
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange({target}) {
+    const {type, name, value} = target;
+  }
+
+  renderTitleInput() {
+    const { title } = this.state;
+    return (
+      <label htmlFor="title-input" data-testid="title-input-label">
+        Título
+        <input
+          name="title"
+          type="text"
+          id="title-input"
+          data-testid="title-input"
+          value={ title }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  renderSubtitleInput() {
+    const { subtitle } = this.state;
+    return (
+      <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+        Subtítulo
+        <input
+          name="subtitle"
+          type="text"
+          id="subtitle-input"
+          data-testid="subtitle-input"
+          value={ subtitle }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  renderImageInput() {
+    const { imagePath } = this.state;
+    return (
+      <label htmlFor="image-input" data-testid="image-input-label">
+        Imagem
+        <input
+          name="imagePath"
+          id="image-input"
+          data-testid="image-input"
+          value={ imagePath }
+          onChange={ this.handleChange }
+          />
+      </label>
+    );
+  }
+
+  renderStoryLine() {
+    const { storyLine } = this.state;
+    return (
+      <label htmlFor="storyline-input" data-testid="storyline-input-label">
+        Sinopse
+        <input
+          name="storyLine"
+          type="text"
+          id="storyline-input"
+          data-testid="storyline-input"
+          value={ storyLine }
+          onChange={ this.handleChange }
+          />
+      </label>
+    );
+  }
+
+  renderRatingInput() {
+    const { rating } = this.state;
+    return (
+      <label htmlFor="rating-input" data-testid="rating-input-label">
+        Avaliação
+        <input
+          name="rating"
+          type="number"
+          id="rating-input"
+          data-testid="rating-input"
+          value={ rating }
+          onChange={ this.handleChange }
+        />
+      </label>
+    );
+  }
+
+  renderGenreInput() {
+    const { genre } = this.state;
+    return (
+      <label htmlFor="genre-input" data-testid="genre-input-label">
+        Gênero
+        <select
+          name="genre"
+          type="number"
+          id="genre-input"
+          data-testid="genre-input"
+          value={ genre }
+          onChange={ this.handleChange }
+        >
+          <option value="action" data-testid="genre-option">Ação</option>
+          <option value="comedy" data-testid="genre-option">Comédia</option>
+          <option value="thriller" data-testid="genre-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  renderButtom() {
+    const { onClick } = this.props;
+    return (
+      <button
+        type="button"
+        onClick{ onClick }
+        data-testid="send-buttom"
+        >
+        Adicionar filme
+        </button>;
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Add Movie</p>
+        <form data-testid="add-movie-form">
+          { this.renderTitleInput()}
+          { this.render.renderSubtitleInput()}
+          { this.renderImageInput()}
+          { this.renderStoryLine()}
+          { this.renderRatingInput()}
+          { this.renderGenreInput() }
+          { this.renderButtom() }
+        </form>
+      </div>
+    );
+  }
+}
+
+AddMovie.prototypes = {
+  onClick: PropTypes.func.isRequired,
+}
+
+export default AddMovie;
