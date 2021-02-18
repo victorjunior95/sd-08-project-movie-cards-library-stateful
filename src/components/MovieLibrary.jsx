2d6c0b69031f6dd41be51e48f1eabbf62527a382
Víctor Cançado <victor.cancado@gmail.com>
2021-02-18 04:08:22 -0300

    Segundo commit

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,25 +1,31 @@
 // implement MovieLibrary component here
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import PropTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// class MovieLibrary extends Component {
+class MovieLibrary extends Component {
 //   constructor(props) {
 //     super(props);
 //   }
 
-//   render() {
-//     return (
-//       <div>
-//         <h2> My awesome movie library </h2>
-//         <SearchBar />
-//         <MovieList movies={this.props.movies} />
-//         <AddMovie />
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={ movies } />
+        <AddMovie />
+      </div>
+    );
+  }
+}
 
-// export default MovieLibrary;
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieLibrary;
