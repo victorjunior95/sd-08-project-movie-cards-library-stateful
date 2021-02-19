6352ccd4a0e5e7c34213087c6dd05aac3fd0db99
Uibirá <usbarreto@gmail.com>
2021-02-19 14:11:23 -0300

    Revisao slint

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,8 +2,8 @@
 // import React, { Component } from 'react';
 import React from 'react';
 import PropTypes from 'prop-types';
-import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
 // import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
