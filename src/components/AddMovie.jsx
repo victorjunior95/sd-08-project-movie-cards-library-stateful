24e1d4336fc49cf834e268067004395f76d21591
Uibirá <usbarreto@gmail.com>
2021-02-19 21:44:42 -0300

    Req.8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,14 +6,13 @@ class AddMovie extends React.Component {
     const { onSearchTextChange } = this.props;
     return (
       <div>
-        Título
         <form data-testid="add-movie-form" method="get" />
         <label data-testid="title-input-label" htmlFor="text-input">
+          Título
           <input
             onChange={ onSearchTextChange }
             data-testid="title-input"
-            type="number"
-            value=""
+            title=""
           />
         </label>
       </div>
