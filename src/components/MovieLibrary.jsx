15437e8191918db5b819e19d2557d615dc0c7b0b
Uibirá <usbarreto@gmail.com>
2021-02-19 21:57:19 -0300

    Req.15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,8 +2,8 @@
 // import React, { Component } from 'react';
 import React from 'react';
 import PropTypes from 'prop-types';
-import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
 // import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
@@ -12,9 +12,6 @@ class MovieLibrary extends React.Component {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        {/* <MovieList movies={this.props.movies} /> */}
-        <AddMovie />
         <p>{movies}</p>
       </div>
     );
