602841f5a1abfd04fb95e6bb5d86bcb0ea37eafe
Cezene <cezenesn@hotmail.com.br>
2021-02-21 18:25:11 -0300

    agora vai

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-import PropTypes from 'prop-types';
 
 class MovieLibrary extends React.Component {
   constructor(props) {
