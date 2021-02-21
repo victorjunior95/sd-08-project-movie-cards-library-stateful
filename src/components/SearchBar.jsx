4aac252eb946e85f76b6e96b866ce5de2d5783dc
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-02-21 15:28:24 -0300

    Resetando o projeto: problema com nodule

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,1 @@
-import React, { Component } from 'react';
-
-class SearchBar extends Component {
-  render() {
-    return (
-      <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" htmlFor="search">
-          Inclui o texto
-          <input data-testid="text-input" type="text" name="" id="search" />
-        </label>
-      </form>);
-  }
-}
-
-export default SearchBar;
+// implement SearchBar component here
