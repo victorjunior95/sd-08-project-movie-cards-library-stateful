aae865ca67ca0ddab9cea7af41257e86a90de4de
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-02-21 16:10:38 -0300

    Correçoes CC

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import Movielibrary from './components/MovieLibrary';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <Movielibrary movies={ movies } /> */}
     </div>
   );
 }
