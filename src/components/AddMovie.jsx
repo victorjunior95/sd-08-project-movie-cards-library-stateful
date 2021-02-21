e128855f7fbe52927dbdf6b94533b80801bfbe2d
Lucas Duque <lucas3rios2014@gmail.com>
2021-02-21 12:26:24 -0300

    renderizar componentes ainda sem funcoes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,9 +3,80 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
+  addMovieTitleInput() {
+    return (
+      <label htmlFor="title-input" data-testid="title-input-label">
+        Titulo
+        <input type="text" data-testid="title-input" />
+      </label>
+    );
+  }
+
+  addMovieSubtitleInput() {
+    return (
+      <label htmlFor="subtitle-input" data-testid="subtitle-input-label">
+        Subtitulo
+        <input type="text" data-testid="subtitle-input" />
+      </label>
+    );
+  }
+
+  addMovieStorylineInput() {
+    return (
+      <label htmlFor="storyline-input" data-testid="storyline-input-label">
+        Sinopse
+        <textarea data-testid="storyline-input" cols="30" rows="10" />
+      </label>
+    );
+  }
+
+  addMovieImageInput() {
+    return (
+      <label htmlFor="image-input" data-testid="image-input-label">
+        Imagen
+        <input type="text" data-testid="image-input" />
+      </label>
+    );
+  }
+
+  addMovieRatingInput() {
+    return (
+      <label htmlFor="rating-input" data-testid="rating-input-label">
+        Avaliação
+        <input type="number" data-testid="rating-input" />
+      </label>
+    );
+  }
+
+  addMovieGenreSelect() {
+    return (
+      <label htmlFor="genre-input" data-testid="genre-input-label">
+        <select data-testid="genre-input">
+          <option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  addMovieButton() {
+    return (
+      <button data-testid="send-button" type="button">Adicionar filme</button>
+    );
+  }
+
   render() {
     return (
-      <span>placeholder</span>
+      <form data-testid="add-movie-form">
+        {this.addMovieTitleInput()}
+        {this.addMovieSubtitleInput()}
+        {this.addMovieGenreSelect()}
+        {this.addMovieStorylineInput()}
+        {this.addMovieImageInput()}
+        {this.addMovieRatingInput()}
+        {this.addMovieButton()}
+      </form>
     );
   }
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,9 +3,45 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  renderFormTextInput() {
+    return (
+      <label htmlFor="text-input" data-testid="text-input-label">
+        Inclui o texto:
+        <input type="text" data-testid="text-input" />
+      </label>
+    );
+  }
+
+  renderFormCheckbox() {
+    return (
+      <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+        Mostrar somente favoritos
+        <input type="checkbox" data-testid="checkbox-input" />
+      </label>
+    );
+  }
+
+  renderFormSelect() {
+    return (
+      <label htmlFor="select-input" data-testid="select-input-label">
+        Filtrar por gênero
+        <select data-testid="select-input">
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     return (
-      <span>placeholder</span>
+      <form data-testid="search-bar-form">
+        {this.renderFormTextInput()}
+        {this.renderFormCheckbox()}
+        {this.renderFormSelect()}
+      </form>
     );
   }
 }
