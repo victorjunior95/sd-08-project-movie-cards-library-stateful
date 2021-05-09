dd0142b392ea650088b93c182ccd7ffb74cab97c
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-05-09 19:45:41 -0300

    Requisito 04

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,25 +1,63 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
-  render() {
+  inputText() {
     const { searchText, onSearchTextChange } = this.props;
     return (
-      <form data-testid="search-bar-form">
-        <label
-          data-testid="text-input-label"
-          htmlFor=""
-        >
-          Inclui o texto:
-          <input
-            type="text"
-            value={ searchText }
-            onChange={ onSearchTextChange }
-            data-testid="text-input"
-          />
-        </label>
+      <label
+        data-testid="text-input-label"
+        htmlFor="text-input"
+      >
+        Inclui o texto:
+        <input
+          data-testid="text-input"
+          id="text-input"
+          onChange={ onSearchTextChange }
+          type="text"
+          value={ searchText }
+        />
+      </label>
+    );
+  }
+
+  inputCheckbox() {
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
+    return (
+      <label
+        data-testid="checkbox-input-label"
+        htmlFor="checkbox-input"
+      >
+        Mostrar somente favoritos
+        <input
+          checked={ bookmarkedOnly }
+          data-testid="checkbox-input"
+          id="checkbox-input"
+          onChange={ onBookmarkedChange }
+          name="checkbox-input"
+          type="checkbox"
+        />
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <form
+        data-testid="search-bar-form"
+      >
+        { this.inputText() }
+        { this.inputCheckbox() }
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
