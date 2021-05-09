36351c78017a6aa1f8f68afa8668b191c016130a
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-05-09 18:29:18 -0300

    Requisito 01

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
-import Header from './components/Header';
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar
+          searchText=""
+          onSearchTextChange={ () => 'ok' }
+          bookmarkedOnly={ false }
+          onBookmarkedChange={ () => 'ok' }
+          selectedGenre=""
+          onSelectedGenreChange={ () => 'ok' }
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
